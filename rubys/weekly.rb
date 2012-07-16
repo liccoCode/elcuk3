fd5ac9199bb26f6c3009f2e5c29c07e8b0d570a7
wyatt <wppurking@gmail.com>
2012-07-16 10:41:12 +0800

    weekly mail fix

--- rubys/weekly.rb
+++ rubys/weekly.rb
@@ -2,13 +2,13 @@
 require "rubygems"
 require "gmail"
 Gmail.connect('support@easyacceu.com', 'Fi0#GR4C^Y') do |gmail|
-	gmail.deliver do
-		to "c@easyacceu.com"
-		#to "wppurking@gmail.com"
-                #[日报] 3.19~3.23每日工作汇报+周总结和下周计划
-		subject "[日报] #{Time.now.strftime('%m.%d')}~#{(Time.now + 604800).strftime('%m.%d')}每日工作汇报+周总结和下周计划"
-		html_part do
-			body "Mailed by ruby script on e.easyacceu.com"
-		end
-	end
+  gmail.deliver do
+    to "c@easyacceu.com,p@easyacceu.com"
+    #to "wppurking@gmail.com"
+    #[日报] 3.19~3.23每日工作汇报+周总结和下周计划
+    subject "[日报] #{Time.now.strftime('%m.%d')}~#{(Time.now + 604800).strftime('%m.%d')}每日工作汇报+周总结和下周计划"
+    html_part do
+      body "Mailed by ruby script on e.easyacceu.com"
+    end
+  end
 end
