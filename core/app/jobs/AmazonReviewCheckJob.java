64a819062359291cfcdc5dc09f30142087a79028
wyatt <wppurking@gmail.com>
2012-11-06 11:57:08 +0800

    refs #670 * Review 更新的 Job 调整. 将 Review 的检查与发现/更新区分开.

---
+++ core/app/jobs/AmazonReviewCheckJob.java
@@ -0,0 +1,42 @@
+package jobs;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import helper.Crawl;
+import models.Jobex;
+import models.market.AmazonListingReview;
+import models.market.Listing;
+import play.jobs.Every;
+import play.jobs.Job;
+
+import java.util.List;
+
+/**
+ * 用来检查系统内的 Amazon Review, 检查是否被删除
+ * * 周期:
+ * - 轮询周期: 5s
+ * - Duration: 3mn
+ * User: wyattpan
+ * Date: 11/6/12
+ * Time: 11:45 AM
+ */
+@Every("5s")
+public class AmazonReviewCheckJob extends Job {
+    @Override
+    public void doJob() {
+        if(!Jobex.findByClassName(AmazonReviewCheckJob.class.getName()).isExcute()) return;
+        // 抓取没有删除, 并且按照最后更新时间的升序排列;
+        // 自己或者其他让的 Listing 都检查.
+        List<AmazonListingReview> reviewsToBeCheck = AmazonListingReview.find("isRemove=false ORDER BY updateAt ASC").fetch(20);
+        for(AmazonListingReview review : reviewsToBeCheck) {
+            JsonElement rvObj = Crawl.crawlReview(Listing.unLid(review.listingId)._2.toString(), review.reviewId);
+            JsonObject obj = rvObj.getAsJsonObject();
+            if(obj.get("isRemove").getAsBoolean()) {
+                review.isRemove = true;
+                review.save();
+            }
+        }
+
+        // TODO 思考对于 Review 还需要有哪些检查?
+    }
+}
--- core/app/jobs/AmazonReviewCrawlJob.java
+++ core/app/jobs/AmazonReviewCrawlJob.java
@@ -1,26 +1,23 @@
 package jobs;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import helper.Crawl;
-import helper.Dates;
 import jobs.works.ListingReviewsWork;
 import models.Jobex;
-import models.market.AmazonListingReview;
 import models.market.Listing;
-import org.joda.time.DateTime;
 import play.jobs.Every;
 import play.jobs.Job;
 
 import java.util.List;
 
 /**
- * Created by IntelliJ IDEA.
+ * 用来发现新的 Reivew 与更新已经存在的 Review
+ * * 周期:
+ * - 轮询周期: 5s
+ * - Duration: 5mn
  * User: wyattpan
  * Date: 9/25/12
  * Time: 9:12 PM
  */
-@Every("10mn")
+@Every("5s")
 public class AmazonReviewCrawlJob extends Job {
     @Override
     public void doJob() {
@@ -28,17 +25,6 @@ public class AmazonReviewCrawlJob extends Job {
         List<Listing> listings = Listing.find("ORDER BY lastUpdateTime").fetch(10);
         for(Listing lst : listings) {
             new ListingReviewsWork(lst.listingId).now();
-        }
-
-        List<AmazonListingReview> reviewsToBeCheck = AmazonListingReview.find("isRemove=false AND (updateAt is NULL OR updateAt<=?)",
-                Dates.night(DateTime.now().minusDays(7).toDate())).fetch(10);
-        for(AmazonListingReview review : reviewsToBeCheck) {
-            JsonElement rvObj = Crawl.crawlReview(Listing.unLid(review.listingId)._2.toString(), review.reviewId);
-            JsonObject obj = rvObj.getAsJsonObject();
-            if(obj.get("isRemove").getAsBoolean()) {
-                review.isRemove = true;
-                review.save();
-            }
         }
     }
 }
