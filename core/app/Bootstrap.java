496f2e829408d33cb20be504e8633f9e3ae8f02b
wyatt <wppurking@gmail.com>
2012-11-15 14:14:12 +0800

    #670 * 暂时不开放, 需要解决 iptables 的问题.

--- core/app/Bootstrap.java
+++ core/app/Bootstrap.java
@@ -39,7 +39,8 @@ public class Bootstrap extends Job {
         Privilege.init();
 
         if(Play.mode.isProd() || (Play.mode.isDev() && "true".equalsIgnoreCase(Play.configuration.getProperty("beanstalkd.dev"))))
-            OsTicketCreateCheck.begin();
+            //OsTicketCreateCheck.begin();
+            System.out.println("暂时不开放");
 
         if(Play.mode.isProd()) {
             Currency.updateCRY();// 系统刚刚启动以后进行一次 Currency 的更新.
