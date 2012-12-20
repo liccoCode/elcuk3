97cfdf21c5f8bdc0e340c59bc2d2a14c797ca518
wyatt <wppurking@gmail.com>
2012-12-20 13:27:08 +0800

    refs #724 * 第一阶段的费用最多 2 条

--- core/test/jobs/promise/FinanceShippedOrdersTest.java
+++ core/test/jobs/promise/FinanceShippedOrdersTest.java
@@ -7,7 +7,6 @@ import models.market.Account;
 import models.market.M;
 import models.market.Orderr;
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import play.test.UnitTest;
 
@@ -23,9 +22,14 @@ import java.util.concurrent.ExecutionException;
  */
 public class FinanceShippedOrdersTest extends UnitTest {
     static String orderId = "002-0031172-2185004";
+    static String orderId2 = "002-4606406-2906621";
 
-    @BeforeClass
-    public static void setUPOrder() throws IOException, ClassNotFoundException {
+
+    /**
+     * 抓取 Amazon 上的 Payment 信息, 第一步; productcharges & amazon
+     */
+    @Test
+    public void testDoJob() throws ExecutionException, InterruptedException, IOException, ClassNotFoundException {
         Orderr ord = new Orderr();
         ord.orderId = orderId;
         ord.state = Orderr.S.SHIPPED;
@@ -33,14 +37,6 @@ public class FinanceShippedOrdersTest extends UnitTest {
         ord.save();
 
         Webs.dev_login(ord.account);
-    }
-
-
-    /**
-     * 抓取 Amazon 上的 Payment 信息, 第一步; productcharges & amazon
-     */
-    @Test
-    public void testDoJob() throws ExecutionException, InterruptedException {
         assertEquals(1, Orderr.count(), 1);
         List<SaleFee> fees = new FinanceShippedOrders(Orderr.<Orderr>findById(orderId)).now().get();
         assertEquals(2, fees.size(), 1);
@@ -50,6 +46,26 @@ public class FinanceShippedOrdersTest extends UnitTest {
 
         assertEquals(fees.get(1).type.name, "amazon");
         assertEquals(fees.get(1).cost, -13.49, 0.01);
+    }
+
+    @Test
+    public void testOrder2() throws IOException, ClassNotFoundException, ExecutionException, InterruptedException {
+        Orderr ord = new Orderr();
+        ord.orderId = orderId2;
+        ord.state = Orderr.S.SHIPPED;
+        ord.account = Account.find("type=?", M.AMAZON_US).first();
+        ord.save();
+
+        Webs.dev_login(ord.account);
+        assertEquals(1, Orderr.count(), 1);
+        List<SaleFee> fees = new FinanceShippedOrders(Orderr.<Orderr>findById(orderId2)).now().get();
+        assertEquals(2, fees.size(), 1);
+        assertEquals("productcharges", fees.get(0).type.name);
+        assertEquals(3.99, fees.get(0).cost, 0.01);
+        assertEquals(Currency.USD, fees.get(0).currency);
+
+        assertEquals("amazon", fees.get(1).type.name);
+        assertEquals(-3.37, fees.get(1).cost, 0.01);
     }
 
     @Test
