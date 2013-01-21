0355797216e89a8e43861f690e27d42898229892
wyatt <wppurking@gmail.com>
2013-01-21 20:31:46 +0800

    fix osticket.rb

--- rubys/osticket.rb
+++ rubys/osticket.rb
@@ -3,13 +3,13 @@ require "httpclient"
 require "gmail"
 
 #7DEE6AF75CC14D852D7DFF8E3156E20E (2.2.4, ruby 1.9.3 (2012-02-16) [i686-linux])
-clnt = HTTPClient.new(:agent_name => 'E1A28384D00F8AB3E8C3582E33D79204')
-state = clnt.get('http://t.easya.cc/api/cron.php').status
+clnt = HTTPClient.new(:agent_name => '77CD3A807B88CD0093940310D1FC4103')
+res = clnt.get('http://t.easya.cc/api/cron.php')
 clnt.receive_timeout = 60000
 
-exit if state == 200
+exit if res.status == 200
 
-puts 'need warnning'
+puts "need warnning #{res.body}"
 #Gmail.connect('wyatt@easyacceu.com', 'hfadantztmbchybf') do |gmail|
 Gmail.connect('support@easyacceu.com', 'Fi0#GR4C^Y') do |gmail|
         gmail.deliver do
@@ -19,7 +19,7 @@ Gmail.connect('support@easyacceu.com', 'Fi0#GR4C^Y') do |gmail|
                 subject "OsTicket Fetch Mail 的线程有错误"
                 html_part do
 			content_type 'text/html; charset=UTF-8'
-                        body "在 e.easyacceu.com 上的触发 OsTicket 抓取邮件的脚本有错误! 请立马检查"
+                        body "在 e.easya.cc 上的触发 OsTicket 抓取邮件的脚本有错误! 请立马检查"
                 end
         end
 end
