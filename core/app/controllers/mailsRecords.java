e99111517d7a6441b41976c64739d8ba960ce2bb
rose <sailingboatlee@gmail.com>
2013-04-07 17:59:41 +0800

    页面曲线图完成 搜索栏有问题

--- core/app/controllers/mailsRecords.java
+++ core/app/controllers/mailsRecords.java
@@ -1,6 +1,11 @@
 package controllers;
 
+import helper.J;
+import helper.Webs;
 import models.MailsRecord;
+import models.view.Ret;
+import models.view.dto.HighChart;
+import play.Logger;
 import play.mvc.Controller;
 
 import java.util.Date;
@@ -14,12 +19,23 @@ import java.util.Date;
 public class MailsRecords extends Controller {
 
     public static void index() {
-           render();
+        Logger.debug("邮件 log   index --------------");
+        render();
     }
 
 
     public static void ajaxRecord(Date from,Date to,String type,boolean success,String group) {
-        MailsRecord.ajaxRecordBy(from, to, type, success, group);
+
+        Logger.info("开始获取 邮件日志....");
+        Logger.info("from:%s  to %s    type=%s   success: %s   group: %s",from,to,type,success,group);
+        try{
+            HighChart  hc= MailsRecord.ajaxRecordBy(from, to, type, success, group);
+            Logger.info("返回结果");
+            renderJSON(J.json(hc));
+        } catch(Exception e) {
+            renderJSON(new Ret(Webs.S(e)));
+        }
+
     }
 
 
--- core/app/models/MailsRecord.java
+++ core/app/models/MailsRecord.java
@@ -10,6 +10,7 @@ import notifiers.Mails;
 import notifiers.SystemMails;
 import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
+import play.Logger;
 import play.cache.*;
 import play.cache.Cache;
 import play.db.jpa.GenericModel;
@@ -33,92 +34,21 @@ public class MailsRecord extends GenericModel {
      @Expose
      public long id;
 
+    /**
+     * 邮件名称
+     */
     @Expose
     public String title;
 
 
-
+    /**
+     * 模板
+     */
     @Expose
     public String template;
 
 
-    public MailsRecord(String title) {
-        this.title=title;
-    }
-    @Cached("5mn")
-    public static HighChart ajaxRecordBy(Date from, Date to, String type, boolean success, String group) {
-        DateTime _from = new DateTime(Dates.morning(from));
-        DateTime _to = new DateTime(Dates.night(to)).plusDays(1);
-        List<MailsRecord> records=getMailsRecords(_from.toDate(), _to.toDate(), type, success, group);
-
-        HighChart lines = new HighChart().startAt(_from.getMillis());
-        DateTime travel = _from.plusDays(0); // copy 一个新的
-
-
-
-        while(travel.getMillis()<_to.getMillis()){
-            String travelStr=travel.toString("yyyy-MM-dd");
-            float totalCount=0;
-
-            //这里有没有更好的方法??????
-            Map<String,Float> counts=new HashMap<String,Float>();
-            if(type.equals(T.NORMAL.toString())){
-                counts.put(Mails.CLEARANCE,0f);
-                counts.put(Mails.REVIEW_US,0f);
-                counts.put(Mails.REVIEW_DE,0f);
-                counts.put(Mails.REVIEW_UK,0f);
-                counts.put(Mails.MORE_OFFERS,0f);
-                counts.put(Mails.FEEDBACK_WARN,0f);
-                counts.put(Mails.FNSKU_CHECK,0f);
-                counts.put(Mails.IS_DONE,0f);
-                counts.put(Mails.REVIEW_WARN,0f);
-            }else if (type.equals(T.FBA.toString())){
-                counts.put(FBAMails.NOT_RECEING,0f);
-                counts.put(FBAMails.RECEIVING_CHECK,0f);
-                counts.put(FBAMails.STATE_CHANGE,0f);
-            }else if (type.equals(T.SYSTEM.toString())){
-                counts.put(SystemMails.DAILY_REVIEW,0f);
-                counts.put(SystemMails.DAILY_FEEDBACK,0f);
-                counts.put(SystemMails.SKU_PIC_CHECK,0f);
-            }
-            for(MailsRecord record : records){
-                DateTime tmp=new DateTime(record.createDt);
-                if(tmp.toString("yyyy-MM-dd").equals(travelStr)){
-                    totalCount+=1;
-                    counts.put(record.template,counts.get(record.template)+1);
-                }
-            }
-            Iterator<String> ite=counts.keySet().iterator();
-            lines.line("all_records").add(totalCount);
-            while(ite.hasNext()){
-                String key=ite.next();
-                lines.line(key).add(counts.get(key));
-            }
-            travel = travel.plusDays(1);
-        }
-        return lines;
-    }
-
-    private static List<MailsRecord> getMailsRecords(Date from, Date to, String type, boolean success, String group) {
-        String cacheKey = Caches.Q.cacheKey(from, to, type, success, group);
-
-        List<MailsRecord> records= Cache.get(cacheKey, List.class);
-        if (records!=null) return records;
-        String HQL="type=? and success=? and createDt between ? and ?";
-        if(!StringUtils.isBlank(group))
-            HQL+=" and recipients like ?";
-
-        if(StringUtils.isBlank(group))
-            records=MailsRecord.find(HQL,type,success,from,to).fetch();
-        else
-            records=MailsRecord.find(HQL,type,success,from,to,group).fetch();
-
-        if(records!=null)
-            Cache.add(cacheKey, records);
-        return records;
-    }
-
-
+    //需不需要用枚举呢?????
     public enum T{
         NORMAL{
             @Override
@@ -147,14 +77,128 @@ public class MailsRecord extends GenericModel {
     @Expose
     public Date createDt=new Date();
 
+    /**
+     * 是否发送成功
+     */
     @Expose
     public boolean success;
 
     @Expose
+    /**
+     * 发件人
+     */
     public String sender;
 
+    /**
+     * 收件人  用 ',' 号隔开
+     */
     @Expose
     public String recipients;
+
+    public MailsRecord(String title) {
+        this.title=title;
+    }
+
+    /**
+     * 条件查询
+     * @param from 开始时间
+     * @param to 结束时间
+     * @param type  类型
+     * @param success
+     * @param group 特定收件人
+     * @return
+     */
+    @Cached("5mn")
+    public static HighChart ajaxRecordBy(Date from, Date to, String type, boolean success, String group) {
+        DateTime _from = new DateTime(Dates.morning(from));
+        DateTime _to = new DateTime(Dates.night(to)).plusDays(1);
+        T t ;
+        //查询当做条件时,也必须 使用枚举类型而不能用String?????
+        if(type.equals(T.NORMAL.toString())){
+             t=T.NORMAL;
+        }else if(type.equals(T.SYSTEM.toString())){
+            t=T.SYSTEM;
+        }else
+            t=T.FBA;
+
+        List<MailsRecord> records=getMailsRecords(_from.toDate(), _to.toDate(),t, success, group);
+
+        HighChart lines = new HighChart().startAt(_from.getMillis());
+        DateTime travel = _from.plusDays(0); // copy 一个新的
+
+        //初始化不同邮件类型 模板的使用次数
+        Map<String,Float> counts=new HashMap<String,Float>();
+        if(type.equals(T.NORMAL.toString())){
+            counts.put(Mails.CLEARANCE,0f);
+            counts.put(Mails.REVIEW_US,0f);
+            counts.put(Mails.REVIEW_DE,0f);
+            counts.put(Mails.REVIEW_UK,0f);
+            counts.put(Mails.MORE_OFFERS,0f);
+            counts.put(Mails.FEEDBACK_WARN,0f);
+            counts.put(Mails.FNSKU_CHECK,0f);
+            counts.put(Mails.IS_DONE,0f);
+            counts.put(Mails.REVIEW_WARN,0f);
+        }else if (type.equals(T.FBA.toString())){
+            counts.put(FBAMails.NOT_RECEING,0f);
+            counts.put(FBAMails.RECEIVING_CHECK,0f);
+            counts.put(FBAMails.STATE_CHANGE,0f);
+        }else if (type.equals(T.SYSTEM.toString())){
+            counts.put(SystemMails.DAILY_REVIEW,0f);
+            counts.put(SystemMails.DAILY_FEEDBACK,0f);
+            counts.put(SystemMails.SKU_PIC_CHECK,0f);
+        }
+
+
+        while(travel.getMillis()<_to.getMillis()){
+            String travelStr=travel.toString("yyyy-MM-dd");
+
+            //总数量
+            float totalCount=0;
+            for(MailsRecord record : records){
+                DateTime tmp=new DateTime(record.createDt);
+                if(tmp.toString("yyyy-MM-dd").equals(travelStr)){
+                    totalCount+=1;
+                    counts.put(record.template,counts.get(record.template)+1);
+                }
+            }
+            lines.line("all_records").add(totalCount);
+            Iterator<String> ite=counts.keySet().iterator();
+            while(ite.hasNext()){
+                String key=ite.next();
+                lines.line(key).add(counts.get(key));
+                counts.put(key,0f);
+            }
+            travel = travel.plusDays(1);
+        }
+        return lines;
+    }
+
+    /**
+     * 查询记录
+     * @param from
+     * @param to
+     * @param type
+     * @param success
+     * @param group
+     * @return
+     */
+    private static List<MailsRecord> getMailsRecords(Date from, Date to, T type, boolean success, String group) {
+        String cacheKey = Caches.Q.cacheKey(from, to, type, success, group);
+        List<MailsRecord> records= Cache.get(cacheKey, List.class);
+        if (records!=null) return records;
+        Logger.info("缓存中无记录,开始查询......");
+        String querystr="type=? and success=? and createDt between ? and ?";
+        if(!StringUtils.isBlank(group)){
+            querystr+=" and recipients like ?";
+            records=MailsRecord.find(querystr,type,success,from,to,"%"+group+"%").fetch();
+        }else
+            records=MailsRecord.find(querystr,type,success,from,to).fetch();
+        if(records!=null)
+            Cache.add(cacheKey, records);
+        return records;
+    }
+
+
 
     /**
      * 根据title查找发送失败的邮件记录
@@ -171,11 +215,10 @@ public class MailsRecord extends GenericModel {
 
    public void addParams(String from, ArrayList<String> recipients, String tmp, T type) {
        this.sender=from;
+       this.recipients="";
        for(String recipient :recipients)
-           if(this.recipients==null)
-               this.recipients=recipient;
-           else
-               this.recipients+=","+recipient;
+           //这里最后会多出个逗号,由于没有从数据库拿出来用的需要,暂时这样
+           this.recipients  +=recipient+",";
        this.template=tmp;
        this.type=type;
    }
