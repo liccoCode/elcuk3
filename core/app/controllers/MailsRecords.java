a322b42b45f810ee91c35c00611fe14b0ca9ac49
rose <sailingboatlee@gmail.com>
2013-04-10 14:49:02 +0800

    重新创建一个基于master的邮件日志功能分支

---
+++ core/app/controllers/MailsRecords.java
@@ -0,0 +1,47 @@
+package controllers;
+
+import helper.J;
+import helper.Webs;
+import models.MailsRecord;
+import models.view.Ret;
+import models.view.dto.HighChart;
+import play.Logger;
+import play.mvc.Controller;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rose
+ * Date: 13-4-3
+ * Time: 下午1:48
+ */
+public class MailsRecords extends Controller {
+
+    public static void index() {
+        render();
+    }
+
+    /**
+     * 查询邮件日志
+     *
+     * @param from
+     * @param to
+     * @param type
+     * @param templates
+     * @param success
+     * @param group
+     */
+    public static void ajaxRecord(Date from, Date to, MailsRecord.T type, List<String> templates, boolean success, String group) {
+        try {
+            renderJSON(J.json(MailsRecord.ajaxRecordBy(from, to, type, templates, success, group)));
+        } catch(Exception e) {
+            e.printStackTrace();
+            renderJSON(new Ret(Webs.S(e)));
+        }
+
+    }
+
+
+}
