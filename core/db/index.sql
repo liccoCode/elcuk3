caba9de1de35be27414c1136d4e50e1d7158b9d0
wyatt <wppurking@gmail.com>
2013-06-13 11:51:18 +0800

    索引的更改.

--- core/db/index.sql
+++ core/db/index.sql
@@ -6,3 +6,9 @@ CREATE INDEX orderr_createdate_state_warnning ON Orderr (createDate, state, warn
 
 # 给 ElcukRecord 的 Fid 添加索引
 CREATE INDEX elcuk_record_fid ON ElcukRecord (fid(14));
+
+# 为 Selling 的 MerchangSKU 添加索引(ajaxSession 等用)
+CREATE INDEX selling_merchantsku ON Selling (merchantSKU(17));
+
+# 首页统计 Ticket 信息用.
+CREATE INDEX ticket_type_state_createAt ON Ticket (type, state, createAt);
