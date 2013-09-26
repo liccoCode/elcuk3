052d80e8cacb032d2241ee1a91fb6ecc4d315416
wyatt <wppurking@gmail.com>
2013-09-26 11:42:45 +0800

    refs #1397 [老的 Feedback 与 Reivew 的后续跟踪追踪] * 完成 Feedback 的更新删除抓取问题 * 编写测试类, 跑通测试.

--- core/app/controllers/Feedbacks.java
+++ core/app/controllers/Feedbacks.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import jobs.FeedbackCheckJob;
+import jobs.FeedbackCrawlJob;
+import models.market.Account;
 import models.market.Feedback;
+import models.market.M;
 import models.product.Category;
 import play.mvc.Controller;
 import play.mvc.With;
@@ -29,13 +32,19 @@ public class Feedbacks extends Controller {
 
     public static void check(String id) {
         Feedback feedback = Feedback.findById(id);
-        String html = FeedbackCheckJob
-                .ajaxLoadFeedbackOnOrderDetailPage(feedback.account, feedback.orderId);
+        String html = FeedbackCheckJob.ajaxLoadFeedbackOnOrderDetailPage(feedback.account, feedback.orderId);
         feedback.isRemove = FeedbackCheckJob.isFeedbackRemove(html);
         if(FeedbackCheckJob.isRequestSuccess(html))
             flash.success("刷新成功. [%s -> %s]", feedback.orderId, feedback.isRemove);
         else
-            flash.error("刷新成功. [%s -> %s]", feedback.orderId, feedback.isRemove);
+            flash.error("刷新失败. [%s -> %s]", feedback.orderId, feedback.isRemove);
         redirect("/feedbacks/show/" + feedback.orderId);
+    }
+
+    public static void fixOnePage(long aid, M m, int p) {
+        long begin = System.currentTimeMillis();
+        Account account = Account.findById(aid);
+        List<Feedback> feedbacks = FeedbackCrawlJob.fetchAccountFeedbackOnePage(account, m, p);
+        renderText("Success: %s, passed: %s ms", feedbacks.size(), System.currentTimeMillis() - begin);
     }
 }
