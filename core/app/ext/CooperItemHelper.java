a29b68e62aa21783fff967639bca3866b08de6bd
mjdylan <mjdyaln@163.com>
2013-10-09 10:01:17 +0800

    #refs  #1429       去处页面两次循环，改成从订单对象中获取，产品要求。

--- core/app/ext/CooperItemHelper.java
+++ core/app/ext/CooperItemHelper.java
@@ -7,19 +7,18 @@ import play.templates.JavaExtensions;
 
 public class CooperItemHelper extends JavaExtensions {
     /**
-     *
-     *格式化产品要求，前台 popover 使用
+     * 格式化产品要求，前台 popover 使用
      */
     public static String formatProductTerms(CooperItem cooperItem) {
         StringBuffer message = new StringBuffer();
         message.append("<span class='label label-info'>产品要求:</span><br>");
-        if( StringUtils.isNotEmpty(cooperItem.productTerms)) {
+        if(StringUtils.isNotEmpty(cooperItem.productTerms)) {
             String[] messageArray = StringUtils.split(cooperItem.productTerms, "\n");
             for(String text : messageArray) {
                 message.append("<p>").append(text).append("<p>");
             }
         }
-        if( StringUtils.isNotEmpty(cooperItem.memo)) {
+        if(StringUtils.isNotEmpty(cooperItem.memo)) {
             message.append("<span class='label label-info'>Memo:</span><br>");
             String[] messageArray = StringUtils.split(cooperItem.memo, "\n");
             for(String text : messageArray) {
