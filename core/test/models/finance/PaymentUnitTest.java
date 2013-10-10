588a85faacfc17d25f67011a33494519e57d39e5
mjdylan <mjdyaln@163.com>
2013-10-10 14:25:26 +0800

    #refs  #1376        增加测试

--- core/test/models/finance/PaymentUnitTest.java
+++ core/test/models/finance/PaymentUnitTest.java
@@ -9,17 +9,17 @@ import models.procure.Cooperator;
 import models.procure.ProcureUnit;
 import models.procure.Shipment;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import play.data.validation.Validation;
 import play.test.UnitTest;
 
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+
 /**
  * 付费明细
- * User: wyatt
- * Date: 7/17/13
- * Time: 11:37 AM
  */
 public class PaymentUnitTest extends UnitTest {
     @Before
@@ -31,6 +31,7 @@ public class PaymentUnitTest extends UnitTest {
     /**
      * 通过 paymentUnit 创建付款单
      */
+    @Ignore
     @Test
     public void testTransportApprove() {
         FactoryBoy.create(Account.class, "de");
@@ -45,18 +46,12 @@ public class PaymentUnitTest extends UnitTest {
             }
         });
 
-        final TransportApply transportApply = new TransportApply();
-        transportApply.serialNumber = "SQK-周伟-008-13";
-        transportApply.save();
-
-        PaymentUnit paymentUnit = FactoryBoy.create(PaymentUnit.class, new BuildCallback<PaymentUnit>() {
+        PaymentUnit paymentUnit = FactoryBoy.create(PaymentUnit.class, "noPayment", new BuildCallback<PaymentUnit>() {
             @Override
             public void build(PaymentUnit target) {
-                target.payment = null;
-                target.amount = 12;
                 target.shipment = FactoryBoy.lastOrCreate(Shipment.class);
                 target.cooperator = FactoryBoy.lastOrCreate(Cooperator.class);
-                target.shipment.apply = transportApply;
+                target.shipment.apply = FactoryBoy.lastOrCreate(TransportApply.class);
             }
         });
 
@@ -64,16 +59,19 @@ public class PaymentUnitTest extends UnitTest {
 
         assertNotNull(paymentUnit.payment);
 
+        assertThat(Payment.count(), is(Long.parseLong("1")));
     }
+
 
     /**
      * 通过 paymentUnit 创建付款单
      * 条件：
-     *  同一个请款单
-     *  同一个工厂
-     *
+     * 同一个请款单
+     * 同一个费用联系人
+     * <p/>
      * 预期结果：共属于同一个payment
      */
+    @Ignore
     @Test
     public void testTransportApproveManyCooperator() {
         FactoryBoy.create(Account.class, "de");
@@ -88,32 +86,72 @@ public class PaymentUnitTest extends UnitTest {
             }
         });
 
-        final TransportApply transportApply = new TransportApply();
-        transportApply.serialNumber = "SQK-周伟-008-13";
-        transportApply.save();
-
         List<PaymentUnit> paymentUnits = FactoryBoy
-                .batchCreate(2, PaymentUnit.class, new SequenceCallback<PaymentUnit>() {
+                .batchCreate(2, PaymentUnit.class, "noPayment", new SequenceCallback<PaymentUnit>() {
                     @Override
                     public void sequence(PaymentUnit target, int i) {
-                        target.payment = null;
-                        target.amount = 12;
                         target.shipment = FactoryBoy.lastOrCreate(Shipment.class);
-
                         target.cooperator = FactoryBoy.lastOrCreate(Cooperator.class);
-                        target.shipment.apply = transportApply;
+                        target.shipment.apply = FactoryBoy.lastOrCreate(TransportApply.class);
                     }
                 });
 
-        PaymentUnit PaymentUnitOne = paymentUnits.get(0);
-        PaymentUnit PaymentUnittwo = paymentUnits.get(1);
+        PaymentUnit paymentUnitOne = paymentUnits.get(0);
+        PaymentUnit paymentUnittwo = paymentUnits.get(1);
 
-        PaymentUnitOne.transportApprove();
-        PaymentUnittwo.transportApprove();
+        paymentUnitOne.transportApprove();
+        paymentUnittwo.transportApprove();
 
-        assertNotNull(PaymentUnitOne.payment);
-        assertNotNull(PaymentUnittwo.payment);
+        assertNotNull(paymentUnitOne.payment);
+        assertNotNull(paymentUnittwo.payment);
 
-        assertEquals(PaymentUnitOne.payment,PaymentUnittwo.payment);
+        assertEquals(paymentUnitOne.payment, paymentUnittwo.payment);
+    }
+
+    /**
+     * 通过 paymentUnit 创建付款单
+     +* 条件：
+     * 同一个请款单sa           ]\
+     * 不同费用联系人
+     * <p/>
+     * 预期结果：不同费用联系人同的不同，产生两条对应的 payment
+     */
+    @Test
+    public void testTransportApproveDifferentCooperator() {
+        FactoryBoy.create(Account.class, "de");
+        FactoryBoy.create(Selling.class, "de");
+        FactoryBoy.create(ProcureUnit.class);
+        FactoryBoy.create(Shipment.class);
+        FactoryBoy.create(PaymentTarget.class);
+         final List<Cooperator> cooperators = FactoryBoy.batchCreate(2, Cooperator.class, "shipper",
+                new SequenceCallback<Cooperator>() {
+                    @Override
+                    public void sequence(Cooperator target, int i) {
+                        target.name = "cooperName_" + i;
+                        target.paymentMethods.add(FactoryBoy.lastOrCreate(PaymentTarget.class));
+                    }
+                });
+
+        List<PaymentUnit> paymentUnits = FactoryBoy
+                .batchCreate(2, PaymentUnit.class, "noPayment", new SequenceCallback<PaymentUnit>() {
+                    @Override
+                    public void sequence(PaymentUnit target, int i) {
+                        // i 初始值为：1
+                        target.cooperator = cooperators.get(i-1);
+                        target.shipment = FactoryBoy.lastOrCreate(Shipment.class);
+                        target.shipment.apply = FactoryBoy.lastOrCreate(TransportApply.class);
+                    }
+                });
+
+        PaymentUnit paymentUnitOne = paymentUnits.get(0);
+        PaymentUnit paymentUnittwo = paymentUnits.get(1);
+
+        paymentUnitOne.transportApprove();
+        paymentUnittwo.transportApprove();
+
+        assertNotNull(paymentUnitOne.payment);
+        assertNotNull(paymentUnittwo.payment);
+
+        assertNotSame(paymentUnitOne.payment, paymentUnittwo.payment);
     }
 }
