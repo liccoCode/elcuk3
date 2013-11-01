012c8a1b701718971a570dd532474c1e14ce4ba4
wyatt <wppurking@gmail.com>
2013-11-01 17:51:46 +0800

    refs #1517 [分析页面的订单统计正确性保证] * 通过 ES 完成对分析页面的数据的统计. * 取消原始的通过 DB 去查询数据的方式. * 核查与 amazon 统计的销量的正确性. * 添加测试类覆盖销量数据的统计正确性.

--- core/app/query/OrderItemQuery.java
+++ core/app/query/OrderItemQuery.java
@@ -3,12 +3,10 @@ package query;
 import helper.DBUtils;
 import models.market.M;
 import models.market.Orderr;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.joda.time.DateTime;
 import play.db.DB;
 import play.db.helper.SqlSelect;
-import play.utils.FastRuntimeException;
 import query.vo.AnalyzeVO;
 
 import java.sql.Connection;
@@ -97,144 +95,6 @@ public class OrderItemQuery {
         return saleMap;
     }
 
-    public Map<String, Integer> analyzeDaySale(Date from, Date to, M market, boolean isSku) {
-        return analyzeDaySale(from, to, market, isSku, DB.getConnection());
-    }
-
-
-    /**
-     * 获取 AnalyzeVO 的门面方法, 内涵处理 all, cateogyrId, sid, sku 的派发
-     *
-     * @param market
-     * @param val
-     * @param type
-     * @param from
-     * @param to
-     * @return
-     */
-    public static List<AnalyzeVO> getAnalyzeVOsFacade(M market, String val, String type, Date from, Date to) {
-        return getAnalyzeVOsFacade(market, val, type, from, to, DB.getConnection());
-    }
-
-    public static List<AnalyzeVO> getAnalyzeVOsFacade(M market, String val, String type, Date from, Date to,
-                                                      Connection conn) {
-        List<AnalyzeVO> lineVos;
-        OrderItemQuery query = new OrderItemQuery();
-        if("all".equals(val)) {
-            lineVos = query.allSalesAndUnits(from, to, market, conn);
-        } else if(val.matches("^\\d{2}$")) {
-            lineVos = query.categorySalesAndUnits(from, to, market, val, conn);
-        } else if("sid".equals(type)) {
-            lineVos = query.sidSalesAndUnits(from, to, market, val, conn);
-        } else if("sku".equals(type)) {
-            lineVos = query.skuSalesAndUnits(from, to, market, val, conn);
-        } else {
-            throw new FastRuntimeException("不支持的类型!");
-        }
-        return lineVos;
-    }
-
-    /**
-     * sid 的销量曲线
-     *
-     * @param from
-     * @param to
-     * @param market
-     * @param sid
-     * @param conn
-     * @return
-     */
-    public List<AnalyzeVO> sidSalesAndUnits(Date from, Date to, M market, String sid, Connection conn) {
-        SqlSelect sql = new SqlSelect()
-                .select("sum(oi.quantity) qty", "sum(oi.usdCost) usdCost",
-                        "DATE_FORMAT(DATE_ADD(oi.createDate, INTERVAL " + market.timeZoneOffset() +
-                                " HOUR), '%Y-%m-%d') as _date")
-                .from("OrderItem oi")
-                .leftJoin("Orderr o ON oi.order_orderId=o.orderId")
-                .leftJoin("Selling s ON oi.selling_sellingId=s.sellingId")
-                .where("s.merchantSKU=?").param(sid)
-                .where("o.createDate>=?").param(market.withTimeZone(from).toDate())
-                .where("o.createDate<=?").param(market.withTimeZone(to).toDate())
-                .where("o.market=?").param(market.name())
-                .where("o.state NOT IN (?,?,?)")
-                .params(Orderr.S.CANCEL.name(), Orderr.S.REFUNDED.name(), Orderr.S.RETURNNEW.name())
-                .groupBy("_date");
-        List<Map<String, Object>> rows = DBUtils.rows(conn, sql.toString(), sql.getParams().toArray());
-        return rows2Vo(rows);
-    }
-
-    /**
-     * sku 的销量曲线
-     *
-     * @param from
-     * @param to
-     * @param market
-     * @param sku
-     * @param conn
-     * @return
-     */
-    public List<AnalyzeVO> skuSalesAndUnits(Date from, Date to, M market, String sku, Connection conn) {
-        SqlSelect sql = new SqlSelect()
-                .select("sum(oi.quantity) as qty", "sum(oi.usdCost) usdCost",
-                        "DATE_FORMAT(DATE_ADD(oi.createDate, INTERVAL " + market.timeZoneOffset() +
-                                " HOUR), '%Y-%m-%d') as _date")
-                .from("OrderItem oi")
-                .leftJoin("Orderr o ON oi.order_orderId=o.orderId")
-                .leftJoin("Product p ON p.sku=oi.product_sku")
-                .where("p.sku=?").param(sku)
-                .where("o.createDate>=?").param(market.withTimeZone(from).toDate())
-                .where("o.createDate<=?").param(market.withTimeZone(to).toDate())
-                .where("o.market=?").param(market.name())
-                .where("o.state NOT IN (?,?,?)")
-                .params(Orderr.S.CANCEL.name(), Orderr.S.REFUNDED.name(), Orderr.S.RETURNNEW.name())
-                .groupBy("_date");
-        List<Map<String, Object>> rows = DBUtils.rows(conn, sql.toString(), sql.getParams().toArray());
-        return rows2Vo(rows);
-    }
-
-    /**
-     * Cateogyr 的销量曲线
-     *
-     * @param from
-     * @param to
-     * @param market
-     * @param categoryId
-     * @param conn
-     * @return
-     */
-    public List<AnalyzeVO> categorySalesAndUnits(Date from, Date to, M market, String categoryId, Connection conn) {
-        SqlSelect sql = new SqlSelect()
-                .select("sum(oi.quantity) qty", "sum(oi.usdCost) usdCost",
-                        "DATE_FORMAT(DATE_ADD(oi.createDate, INTERVAL " + market.timeZoneOffset() +
-                                " HOUR), '%Y-%m-%d') as _date")
-                .from("OrderItem oi")
-                .leftJoin("Orderr o ON oi.order_orderId=o.orderId");
-        if(StringUtils.isNotBlank(categoryId))
-            sql.leftJoin("Product p ON p.sku=oi.product_sku")
-                    .where("p.category_categoryId=?").param(categoryId);
-        sql.where("o.createDate>=?").param(market.withTimeZone(from).toDate())
-                .where("o.createDate<=?").param(market.withTimeZone(to).toDate())
-                .where("o.market=?").param(market.name())
-                .where("o.state IN (?,?,?)")
-                .params(Orderr.S.PAYMENT.name(), Orderr.S.PENDING.name(), Orderr.S.SHIPPED.name())
-                .groupBy("_date");
-
-        List<Map<String, Object>> rows = DBUtils.rows(conn, sql.toString(), sql.getParams().toArray());
-        return rows2Vo(rows);
-    }
-
-    /**
-     * 所有产品的销量曲线
-     *
-     * @param from
-     * @param to
-     * @param market
-     * @param conn
-     * @return
-     */
-    public List<AnalyzeVO> allSalesAndUnits(Date from, Date to, M market, Connection conn) {
-        return categorySalesAndUnits(from, to, market, null, conn);
-    }
 
     private List<AnalyzeVO> rows2Vo(List<Map<String, Object>> rows) {
         List<AnalyzeVO> items = new ArrayList<AnalyzeVO>();
