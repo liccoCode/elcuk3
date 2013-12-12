254291789ed19a6ca2e73128732be30cdb8a6882
wyatt <wppurking@gmail.com>
2013-12-12 17:14:51 +0800

    * 提供可以通过 FBAShipmentID 搜索采购计划.

--- core/app/models/view/post/ProcurePost.java
+++ core/app/models/view/post/ProcurePost.java
@@ -146,9 +146,10 @@ public class ProcurePost extends Post<ProcureUnit> {
                 String word = this.word();
                 sbd.append(" AND (")
                         .append("product.sku LIKE ? OR ")
-                        .append("selling.sellingId LIKE ?")
+                        .append("selling.sellingId LIKE ? OR ")
+                        .append("fba.shipmentId LIKE ?")
                         .append(") ");
-                for(int i = 0; i < 2; i++) params.add(word);
+                for(int i = 0; i < 3; i++) params.add(word);
             }
         }
 
