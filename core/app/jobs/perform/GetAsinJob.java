520a9299ba44939de16d13befc660687a02c9e7f
wyatt <wppurking@gmail.com>
2013-12-24 11:33:39 +0800

    * 解决最终的成功获取 ASIN 的问题. -> 考虑 selling 与 listing 的外键约束问题.

--- core/app/jobs/perform/GetAsinJob.java
+++ core/app/jobs/perform/GetAsinJob.java
@@ -1,6 +1,7 @@
 package jobs.perform;
 
 import ext.LinkHelper;
+import helper.Constant;
 import helper.DBUtils;
 import helper.HTTP;
 import jobs.driver.BaseJob;
@@ -15,6 +16,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import play.Play;
 import play.utils.FastRuntimeException;
 
 import java.io.File;
@@ -40,13 +42,13 @@ public class GetAsinJob extends BaseJob {
          * 2. 获取 UPS 去搜索对应的Listing, 并获取返回的 html 页面
          * 3. 处理 html 结果(两种情况: 没有搜索到结果 搜索到正确的一条结果)
          */
-        if (getContext().get("account.id") == null)
+        if(getContext().get("account.id") == null)
             throw new FastRuntimeException("没有提交 account.id 信息, 不知道使用哪个销售账户去登陆.");
-        if (getContext().get("sellingId") == null)
+        if(getContext().get("sellingId") == null)
             throw new FastRuntimeException("没有提交 sellingId 信息");
-        if (getContext().get("excuteCount") != null) {
+        if(getContext().get("excuteCount") != null) {
             excuteCount = NumberUtils.toInt(getContext().get("excuteCount").toString());
-            if ("3".contains(getContext().get("excuteCount").toString())) {
+            if("3".contains(getContext().get("excuteCount").toString())) {
                 throw new FastRuntimeException("未找到合适的 ASIN 数据！");
             }
         }
@@ -55,38 +57,47 @@ public class GetAsinJob extends BaseJob {
         String sellingId = getContext().get("sellingId").toString();
         Selling selling = Selling.findById(sellingId);
         Document doc = Jsoup.parse(HTTP.get(account.cookieStore(), LinkHelper.searchAsinByUPCLink(selling)));
-        File file = new File(String.format("%s/%s", "/Users/mac/Desktop", "url" + System.currentTimeMillis()));
-        try {
-            FileUtils.write(file, doc.outerHtml(), "ISO8859-1");
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(Play.mode.isDev()) {
+            try {
+                File file = new File(String.format("%s/deploy/%s", Constant.E_LOGS, "url" + System.currentTimeMillis()));
+                FileUtils.write(file, doc.outerHtml(), "ISO8859-1");
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
         }
         Map nextContext = getContext();
         String asin = null;
         try {
             Elements tables = doc.select("table.manageTable");
             Elements trs = tables.select("tr");
-            for (Element tr : trs) {
+            for(Element tr : trs) {
                 String merchantSKU = tr.select("td:eq(4)").text();
                 asin = tr.select("td:eq(5)").select("a").text();
-                if (asin.length() == 10) {
+                if(asin.length() == 10) {
                     /**
                      * 1. 验证抓取的 SKU 与数据库内的 Selling 对象的 SKU 是否匹配
                      * 2. 更新 Listing 的Id (更改为 [asin]_[market])
                      * 3. 更新 Selling 的 ASIN 值
                      */
-                    if (StringUtils.containsIgnoreCase(selling.merchantSKU, merchantSKU)) {
+                    if(StringUtils.containsIgnoreCase(selling.merchantSKU, merchantSKU)) {
                         selling.asin = asin;
-                        DBUtils.execute(String.format("update Listing set listingId='%s' , asin='%s' where listingId='%s'",
-                                String.format("%s_%s", asin, selling.market.toString()),
-                                asin,
-                                String.format("%s_%s", selling.aps.upc, selling.market.toString())
-                        ));
                         selling.save();
+                        // 因为外键约束, 所以这里需要关闭 MySQL 的外键检查, 然后再进行 listingId 切换,
+                        // 同时需要将直接关联的 Selling.listing_listingId 外键进行更换.
+                        // TODO: 因为 Listing 可能会涉及到很多其他的外键关联, 是否考虑将 listingId 更换为 UPC_Market?
+                        DBUtils.execute("SET foreign_key_checks=0");
+                        String listingId = String.format("%s_%s", asin, selling.market.toString());
+                        DBUtils.execute(String.format("UPDATE Selling SET listing_listingId='%s' WHERE sellingId='%s'",
+                                listingId, selling.sellingId));
+                        DBUtils.execute(
+                                String.format("UPDATE Listing SET listingId='%s' , asin='%s' WHERE listingId='%s'",
+                                        listingId, asin,
+                                        String.format("%s_%s", selling.aps.upc, selling.market.toString())));
+                        DBUtils.execute("SET foreign_key_checks=1");
                     }
                 }
             }
-        } catch (Exception e) {
+        } catch(Exception e) {
             /**
              * 1. 出现异常，重新添加一个新的job任务
              * 2. 将计数器加1
@@ -96,7 +107,7 @@ public class GetAsinJob extends BaseJob {
             GJob.perform(GetAsinJob.class.getName(), nextContext, DateTime.now().plusMinutes(2).toDate());
             throw new FastRuntimeException("解析html文档时发生异常");
         }
-        if (asin == null || "".contains(asin.trim())) {
+        if(asin == null || "".contains(asin.trim())) {
             ++excuteCount;
             nextContext.put("excuteCount", excuteCount);
             GJob.perform(GetAsinJob.class.getName(), nextContext, DateTime.now().plusMinutes(2).toDate());
