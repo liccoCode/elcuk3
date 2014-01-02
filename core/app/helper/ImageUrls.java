f9e76e80eab1621f121aed386203d643aa40cc4e
duan <duan@easya.cc>
2014-01-02 11:53:49 +0800

    修改所有市场模板文件的isGiftWrap字段位置，并完成测试product上架功能。

--- core/app/helper/ImageUrls.java
+++ core/app/helper/ImageUrls.java
@@ -32,7 +32,7 @@ public class ImageUrls extends JavaExtensions {
                 returnStr = imageUrl + imageNames[target];
             }
         } else if(imageNames == null && target == 0) {
-            returnStr = "https://raw.github.com/duanhongbing/elcuk2/master/amazon-logo-10-300x300.png";
+            returnStr = Play.configuration.getProperty("application.baseUrl")+"images/amazon-logo.png";
         }
         return returnStr;
     }
