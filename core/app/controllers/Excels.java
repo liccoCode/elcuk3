e53e470f73548cbd6a3dce91df08e2e2646b9300
duan <duan@easya.cc>
2014-02-17 17:58:22 +0800

    采购单页面增加导出EXCEL功能

--- core/app/controllers/Excels.java
+++ core/app/controllers/Excels.java
@@ -25,4 +25,20 @@ public class Excels extends Controller {
         render(excel);
     }
 
+    /**
+     * 下载采购单综合Excel表格
+     */
+    public static void deliveryments(DeliveryPost p) {
+        List<Deliveryment> deliverymentList = p.query();
+        if(deliverymentList != null && deliverymentList.size() != 0) {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+            request.format = "xls";
+            renderArgs.put(RenderExcel.RA_FILENAME,
+                    String.format("%s-%s采购单.xls", formatter.format(p.from), formatter.format(p.to)));
+            renderArgs.put(RenderExcel.RA_ASYNC, false);
+            render(deliverymentList);
+        } else {
+            renderText("没有数据无法生成Excel文件！");
+        }
+    }
 }
