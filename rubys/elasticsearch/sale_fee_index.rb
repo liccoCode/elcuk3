836d9e4f9c49fb7da7dd06b0891f6651f7110e7b
wyatt <wppurking@gmail.com>
2014-03-05 16:31:07 +0800

    update to 2014

--- rubys/elasticsearch/sale_fee_index.rb
+++ rubys/elasticsearch/sale_fee_index.rb
@@ -4,8 +4,9 @@ require "multi_json"
 
 
 class ESWorker < Thread
-	def initialize(dcs)
-		super(dcs) do |docs|
+	def initialize(dcs, i)
+		super(dcs, i) do |docs, i|
+      print "Index SaleFee ... #{i} / 4326633\r"
 			post_data = ""
 			docs.each do |doc|
 				post_data << MultiJson.dump({index: {_index: "elcuk2", _type: "salefee", _id: doc["salefeeId"]}}) << "\n"
@@ -19,7 +20,7 @@ end
 
 # SQL 语句没有处理一个订单有多个不同 SKU 产品的问题
 SQL = %q(SELECT s.id salefeeId, s.`date`, s.market, s.account_id, s.usdCost costInUSD, s.type_name, oi.selling_sellingId, oi.product_sku FROM SaleFee s
- left join OrderItem oi ON oi.order_orderId=s.order_orderId where s.date>='2013-12-30')
+ left join OrderItem oi ON oi.order_orderId=s.order_orderId where s.date>='2014-01-01')
 
 clt = Mysql2::Client.new(host: 'aggron.easya.cc', username: 'root', password: 'crater10lake', database: 'elcuk2')
 
@@ -32,13 +33,11 @@ rs.each do |row|
 	row['date'] = row['date'].utc.iso8601
 	docs << row
 	if i % 2000 == 0
-		ESWorker.new(docs.dup)
+		ESWorker.new(docs.dup, i)
 		docs.clear
 	end
-	print "Index SaleFee ... #{i} / 4326633\r"
-	#print "Index SaleFee ... #{i} / 3034267\r"
 end
-ESWorker.new(docs.dup) if docs.size > 0
+ESWorker.new(docs.dup, i) if docs.size > 0
 puts "----------------------"
 # 等待 20 s, 等待后面的线程执行完成
 sleep(20)
