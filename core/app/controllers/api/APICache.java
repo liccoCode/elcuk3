f7524231a6527b833d50d3c79a88d52a19325469
duan <duan@easya.cc>
2014-04-01 14:52:05 +0800

    重命名

--- core/app/controllers/api/APICache.java
+++ core/app/controllers/api/APICache.java
@@ -2,11 +2,7 @@ package controllers.api;
 
 import models.view.Ret;
 import play.cache.Cache;
-import play.db.jpa.JPA;
 import play.mvc.Controller;
-import play.mvc.With;
-
-import java.util.List;
 
 /**
  * ES执行后需要清理缓存，保证数据及时
@@ -14,13 +10,12 @@ import java.util.List;
  * Date: 14-3-27
  * Time: 上午10:12
  */
-@With({APIChecker.class})
 public class APICache extends Controller {
     /**
      * es执行完后清理缓存
      */
     public static void esCacheClear() {
         Cache.clear();
-        renderJSON(new Ret(true,"清理缓存成功!"));
+        renderJSON(new Ret(true, "清理缓存成功!"));
     }
 }
