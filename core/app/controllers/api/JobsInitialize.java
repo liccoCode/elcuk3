e1f26964a3dd9f291a4751d4ffbf438a324bb43c
caryeasya <cary@easya.cc>
2014-04-14 10:34:21 +0800

    修改job

--- core/app/controllers/api/JobsInitialize.java
+++ core/app/controllers/api/JobsInitialize.java
@@ -2,8 +2,8 @@ package controllers.api;
 
 import jobs.PmDashboard.AbnormalFetchJob;
 import jobs.categoryInfo.CategoryInfoFetchJob;
+import jobs.PmDashboard.PmDashboardFetchJob;
 import jobs.driver.GJob;
-import play.cache.Cache;
 import play.mvc.Controller;
 import play.mvc.With;
 
@@ -21,7 +21,7 @@ public class JobsInitialize extends Controller {
 
     /**
      * PM 首页异常处理 Job
-     * 周期：一天处理1到两次
+     * 周期：一天处理1次  凌晨 8点30执行
      */
     public static void initAbnormalFetchJob() {
         GJob.perform(AbnormalFetchJob.class.getName(), new HashMap<String, Object>());
@@ -29,9 +29,17 @@ public class JobsInitialize extends Controller {
 
     /**
      * CategoryInfo 数据处理 Job
-     * 周期：一天处理1到两次
+     * 周期：一天处理1次  凌晨 8点40执行
      */
     public static void initCategoryInfoFetchJob() {
         GJob.perform(CategoryInfoFetchJob.class.getName(), new HashMap<String, Object>());
+    }
+
+    /**
+     * 利润目标 数据处理 Job
+     * 周期：一天处理1次 ,凌晨 3点执行
+     */
+    public static void initTargetInfoFetchJob() {
+        GJob.perform(PmDashboardFetchJob.class.getName(), new HashMap<String, Object>());
     }
 }
