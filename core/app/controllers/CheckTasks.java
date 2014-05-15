f9e251f7d5afa40ab2d96ae60affde4b269d9948
caryeasya <cary@easya.cc>
2014-05-15 09:19:38 +0800

    质检任务列表

--- core/app/controllers/CheckTasks.java
+++ core/app/controllers/CheckTasks.java
@@ -4,10 +4,12 @@ import models.procure.Cooperator;
 import models.product.Whouse;
 import models.qc.CheckTask;
 import models.view.post.CheckTaskPost;
+import org.joda.time.DateTime;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -28,10 +30,20 @@ public class CheckTasks extends Controller {
 
 
     @Check("checktasks.checklist")
-    public static void checklist(CheckTaskPost p) {
+    public static void checklist(CheckTaskPost p, int day) {
         CheckTask.generateTask();
         List<CheckTask> tasklist = null;
         if(p == null) p = new CheckTaskPost();
+        if(day == 3) {
+            p.from = DateTime.now().minusDays(3).toDate();
+            p.to = new Date();
+        } else if(day == 2) {
+            p.from = DateTime.now().minusDays(2).toDate();
+            p.to = new Date();
+        } else if(day == 1) {
+            p.from = DateTime.now().minusDays(1).toDate();
+            p.to = new Date();
+        }
         tasklist = p.query();
         render(tasklist, p);
     }
