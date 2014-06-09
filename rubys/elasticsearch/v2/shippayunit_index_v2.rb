283de8d8589a31042c5128167ca5faf62290f878
duan <duan@easya.cc>
2014-06-09 16:33:12 +0800

    * 修改ES shippayunit index 索引ruby文件

--- rubys/elasticsearch/v2/shippayunit_index_v2.rb
+++ rubys/elasticsearch/v2/shippayunit_index_v2.rb
@@ -71,7 +71,7 @@ class ShipPayUnitActor
   def shipitem_relate(shipitem_id)
     # [{sku: xx, selling_id: yy}, {sku: zz, selling_id: cc}]
     row_gorup = {sku: [], selling_id: [], weight: 0}
-    DB["SELECT pu.product_sku sku, pu.selling_sellingId selling_id, pro.weight*pu.qty weight FROM ShipItem si LEFT JOIN ProcureUnit pu ON si.unit_id=pu.id LEFT JOIN Product pro on pu.product_sku = pro.sku WHERE si.id=?", shipitem_id].each do |row|
+    DB["SELECT pu.product_sku sku, pu.selling_sellingId selling_id, (CASE WHEN pu.qty>0 THEN pro.weight*pu.qty WHEN pu.qty=0 THEN pro.weight*pu.planQty WHEN pu.qty IS NULL THEN pro.weight*pu.planQty END) weight FROM ShipItem si LEFT JOIN ProcureUnit pu ON si.unit_id=pu.id LEFT JOIN Product pro on pu.product_sku = pro.sku WHERE si.id=?", shipitem_id].each do |row|
       row_gorup[:sku] << row[:sku] unless row_gorup[:sku].include?(row[:sku])
       row_gorup[:selling_id] << row[:selling_id] unless row_gorup[:selling_id].include?(row[:selling_id])
       row_gorup[:weight] += row[:weight] unless row[:weight] == nil or row[:weight] == "NULL"
@@ -83,7 +83,7 @@ class ShipPayUnitActor
   def shipment(shipment_id)
     unless @shipment_hash.key?(shipment_id)
       row_gorup = {sku: [], selling_id: [], weight: 0}
-      DB["SELECT pu.product_sku sku, pu.selling_sellingId selling_id, pro.weight*pu.qty weight FROM ShipItem si LEFT JOIN Shipment s ON si.shipment_id=s.id LEFT JOIN ProcureUnit pu ON si.unit_id=pu.id LEFT JOIN Product pro on pu.product_sku = pro.sku WHERE si.shipment_id=?", shipment_id].each do |row|
+      DB["SELECT pu.product_sku sku, pu.selling_sellingId selling_id, (CASE WHEN pu.qty>0 THEN pro.weight*pu.qty WHEN pu.qty=0 THEN pro.weight*pu.planQty WHEN pu.qty IS NULL THEN pro.weight*pu.planQty END) weight FROM ShipItem si LEFT JOIN Shipment s ON si.shipment_id=s.id LEFT JOIN ProcureUnit pu ON si.unit_id=pu.id LEFT JOIN Product pro on pu.product_sku = pro.sku WHERE si.shipment_id=?", shipment_id].each do |row|
         row_gorup[:sku] << row[:sku] unless row_gorup[:sku].include?(row[:sku])
         row_gorup[:selling_id] << row[:selling_id] unless row_gorup[:selling_id].include?(row[:selling_id])
         row_gorup[:weight] += row[:weight] unless row[:weight] == nil or row[:weight] == "NULL"
