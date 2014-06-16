95570f42e26b8005ec4f207644af1f365666d996
duan <duan@easya.cc>
2014-06-16 17:50:05 +0800

    * 销售统计报表计算采用Play Job形式，便于将请求能够保存不丢失

--- core/app/jobs/SaleReportsJob.java
+++ core/app/jobs/SaleReportsJob.java
@@ -1,22 +1,17 @@
 package jobs;
 
-import controllers.Login;
-import controllers.Secure;
 import helper.Caches;
 import helper.LogUtils;
 import jobs.driver.BaseJob;
 import models.User;
 import models.market.M;
 import models.market.Selling;
-import models.product.Category;
 import models.view.dto.SaleReportDTO;
 import org.apache.commons.lang.StringUtils;
 import play.cache.Cache;
-import play.utils.FastRuntimeException;
 import services.MetricSaleReportService;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -33,37 +28,24 @@ import java.util.regex.Pattern;
 public class SaleReportsJob extends BaseJob {
     private MetricSaleReportService service = new MetricSaleReportService();
 
-    /**
-     * 匹配 SKU
-     */
-    public final Pattern SKU = Pattern.compile("^sku:([0-9a-zA-Z]+-[0-9a-zA-Z]+-?[0-9a-zA-Z]*)");
-
-    /**
-     * 匹配 Selling
-     */
-    public final Pattern SELLING = Pattern.compile("^selling:\\w*");
-
-    /**
-     * 匹配 Category
-     */
-    public final Pattern CATEGORY = Pattern.compile("^category:(\\d*)");
-
     public Date from;
     public Date to;
     public M market;
     public String search;
-
     /**
      * Job执行时获取不到对应的登陆信息，只好手动传递过来好了，囧
      */
     public String username;
 
-    public SaleReportsJob(Date from, Date to, M market, String search, String username) {
+    public List<String> sellingIds;
+
+    public SaleReportsJob(Date from, Date to, M market, String search, String username, List<String> sellingIds) {
         this.from = from;
         this.to = to;
         this.market = market;
         this.search = search;
         this.username = username;
+        this.sellingIds = sellingIds;
     }
 
     public static boolean isRunning(String key) {
@@ -71,55 +53,16 @@ public class SaleReportsJob extends BaseJob {
     }
 
     public String buildKey() {
-        User user = User.findByUserName(username);
         return Caches.Q.cacheKey(this.from, this.to, (this.market != null ? this.market : "AllMarket"),
-                (this.search != null ? this.search : ""), user.username, "SaleReports");
+                (this.search != null ? this.search : ""), username, "SaleReports");
     }
 
-    public List<String> params() {
-        User user = User.findByUserName(username);
-        //category 权限
-        List<String> categorys = User.getTeamCategorys(user);
-        //sku 权限
-        List<String> skus = User.getSkus(user);
-        //selling 权限
-        List<String> sellings = User.getSellings(user);
-
-        String categoryId = isSearchCategory();
-        if(StringUtils.isNotBlank(categoryId)) {
-            if(!categorys.contains(categoryId)) {
-                throw new FastRuntimeException(String.format("Sorry,没有查看 Category: %s 销售数据的权限", categoryId));
-            }
-            skus = Category.getSKUs(categoryId);
-            return Selling.sids(skus);
-        }
-
-        String sku = isSearchSKU();
-        if(StringUtils.isNotBlank(sku)) {
-            if(!skus.contains(sku)) {
-                throw new FastRuntimeException(String.format("Sorry,没有查看 SKU: %s 销售数据的权限", sku));
-            }
-            return Selling.sids(sku);
-
-        }
-        String sellingId = isSearchSelling();
-        if(StringUtils.isNotBlank(sellingId)) {
-            if(!sellings.contains(sellingId)) {
-                throw new FastRuntimeException(String.format("Sorry,没有查看 Selling: %s 销售数据的权限", sellingId));
-            }
-            return Arrays.asList(sellingId);
-        }
-        return sellings;
-    }
 
     @SuppressWarnings("unchecked")
     @Override
     public void doit() {
         long begin = System.currentTimeMillis();
 
-        List<String> sellingIds = params();
-        if(this.market != null)
-            org.apache.commons.collections.CollectionUtils.filter(sellingIds, new SellingIdPredicate());
 
         String runningKey = buildKey() + "_running";
         Cache.add(runningKey, runningKey);
@@ -147,53 +90,5 @@ public class SaleReportsJob extends BaseJob {
         Cache.delete(runningKey);
         LogUtils.JOBLOG.info(String.format("SaleReportPost execute with key: %s, Calculate time: %s", buildKey(),
                 System.currentTimeMillis() - begin));
-    }
-
-
-    /**
-     * 根据正则表达式搜索是否有类似 sku:123 这样的搜索如果有则直接进行 sku 搜索
-     *
-     * @return
-     */
-    public String isSearchSKU() {
-        if(StringUtils.isNotBlank(this.search)) {
-            Matcher matcher = SKU.matcher(this.search);
-            if(matcher.find()) return matcher.group(1);
-        }
-        return null;
-    }
-
-    /**
-     * 根据正则表达式搜索是否有类似 selling:123 这样的搜索如果有则直接进行 selling 搜索
-     *
-     * @return
-     */
-    public String isSearchSelling() {
-        if(StringUtils.isNotBlank(this.search)) {
-            Matcher matcher = SELLING.matcher(this.search);
-            if(matcher.find()) return StringUtils.split(this.search, "selling:")[0];
-        }
-        return null;
-    }
-
-    /**
-     * 根据正则表达式搜索是否有类似 category:123 这样的搜索如果有则直接进行 category 搜索
-     *
-     * @return
-     */
-    public String isSearchCategory() {
-        if(StringUtils.isNotBlank(this.search)) {
-            Matcher matcher = CATEGORY.matcher(this.search);
-            if(matcher.find()) return matcher.group(1);
-        }
-        return null;
-    }
-
-    private class SellingIdPredicate implements org.apache.commons.collections.Predicate {
-        @Override
-        public boolean evaluate(Object o) {
-            String sellingId = (String) o;
-            return StringUtils.endsWithIgnoreCase(Selling.sidToMarket(sellingId).name(), market.name());
-        }
     }
 }
--- core/app/models/view/post/SaleReportPost.java
+++ core/app/models/view/post/SaleReportPost.java
@@ -29,15 +29,26 @@ import java.util.regex.Pattern;
  * Time: AM10:17
  */
 public class SaleReportPost {
+
+    /**
+     * 匹配 SKU
+     */
+    public final Pattern SKU = Pattern.compile("^sku:([0-9a-zA-Z]+-[0-9a-zA-Z]+-?[0-9a-zA-Z]*)");
+
+    /**
+     * 匹配 Selling
+     */
+    public final Pattern SELLING = Pattern.compile("^selling:\\w*");
+
+    /**
+     * 匹配 Category
+     */
+    public final Pattern CATEGORY = Pattern.compile("^category:(\\d*)");
+
     public Date from;
     public Date to;
     public M market;
     public String search;
-
-    /**
-     * 拥有权限的Selling集合
-     */
-    private List<String> sellingIds;
 
     public SaleReportPost() {
         DateTime now = DateTime.now().withTimeAtStartOfDay();
@@ -46,18 +57,105 @@ public class SaleReportPost {
 
     }
 
+    public List<String> params() {
+        User user = Login.current();
+        //category 权限
+        List<String> categorys = User.getTeamCategorys(user);
+        //sku 权限
+        List<String> skus = User.getSkus(user);
+        //selling 权限
+        List<String> sellings = User.getSellings(user);
+
+        String categoryId = isSearchCategory();
+        if(StringUtils.isNotBlank(categoryId)) {
+            if(!categorys.contains(categoryId)) {
+                throw new FastRuntimeException(String.format("Sorry,没有查看 Category: %s 销售数据的权限", categoryId));
+            }
+            skus = Category.getSKUs(categoryId);
+            return Selling.sids(skus);
+        }
+
+        String sku = isSearchSKU();
+        if(StringUtils.isNotBlank(sku)) {
+            if(!skus.contains(sku)) {
+                throw new FastRuntimeException(String.format("Sorry,没有查看 SKU: %s 销售数据的权限", sku));
+            }
+            return Selling.sids(sku);
+
+        }
+        String sellingId = isSearchSelling();
+        if(StringUtils.isNotBlank(sellingId)) {
+            if(!sellings.contains(sellingId)) {
+                throw new FastRuntimeException(String.format("Sorry,没有查看 Selling: %s 销售数据的权限", sellingId));
+            }
+            return Arrays.asList(sellingId);
+        }
+        return sellings;
+    }
+
     public List<SaleReportDTO> query() {
-        User user = User.current();
+        User user = Login.current();
         String key = Caches.Q.cacheKey(this.from, this.to, (this.market != null ? this.market : "AllMarket"),
                 (this.search != null ? this.search : ""), user.username, "SaleReports");
 
         List<SaleReportDTO> dtos = Cache.get(key, List.class);
         if(dtos == null) {
             if(!SaleReportsJob.isRunning(key)) {
-                new SaleReportsJob(this.from, this.to, this.market, this.search, user.username).now();
+                List<String> sellingIds = params();
+                if(this.market != null) {
+                    org.apache.commons.collections.CollectionUtils.filter(sellingIds, new SellingIdPredicate());
+                }
+                new SaleReportsJob(this.from, this.to, this.market, this.search, user.username, sellingIds).now();
             }
             throw new FastRuntimeException("已经在后台计算中，请于 10min 后再来查看结果~");
         }
         return dtos;
+    }
+
+    /**
+     * 根据正则表达式搜索是否有类似 sku:123 这样的搜索如果有则直接进行 sku 搜索
+     *
+     * @return
+     */
+    public String isSearchSKU() {
+        if(StringUtils.isNotBlank(this.search)) {
+            Matcher matcher = SKU.matcher(this.search);
+            if(matcher.find()) return matcher.group(1);
+        }
+        return null;
+    }
+
+    /**
+     * 根据正则表达式搜索是否有类似 selling:123 这样的搜索如果有则直接进行 selling 搜索
+     *
+     * @return
+     */
+    public String isSearchSelling() {
+        if(StringUtils.isNotBlank(this.search)) {
+            Matcher matcher = SELLING.matcher(this.search);
+            if(matcher.find()) return StringUtils.split(this.search, "selling:")[0];
+        }
+        return null;
+    }
+
+    /**
+     * 根据正则表达式搜索是否有类似 category:123 这样的搜索如果有则直接进行 category 搜索
+     *
+     * @return
+     */
+    public String isSearchCategory() {
+        if(StringUtils.isNotBlank(this.search)) {
+            Matcher matcher = CATEGORY.matcher(this.search);
+            if(matcher.find()) return matcher.group(1);
+        }
+        return null;
+    }
+
+    private class SellingIdPredicate implements org.apache.commons.collections.Predicate {
+        @Override
+        public boolean evaluate(Object o) {
+            String sellingId = (String) o;
+            return StringUtils.endsWithIgnoreCase(Selling.sidToMarket(sellingId).name(), market.name());
+        }
     }
 }
