b4c5d7b35d65d1944e4ffb078afbd9da50ed004e
caryeasya <cary@easya.cc>
2014-06-17 17:21:01 +0800

    修改导出session

--- core/app/models/view/post/TrafficRatePost.java
+++ core/app/models/view/post/TrafficRatePost.java
@@ -71,8 +71,14 @@ public class TrafficRatePost extends Post<TrafficRate> {
 
         sbd.append("date").append(">=?").append(" AND ")
                 .append("date<=?");
-        params.add(Dates.morning(this.from));
-        params.add(Dates.night(this.to));
+        if(this.market != null) {
+            params.add(market.withTimeZone(Dates.morning(this.from)));
+            params.add(market.withTimeZone(Dates.morning(this.to)));
+        } else {
+            params.add(Dates.morning(this.from));
+            params.add(Dates.night(this.to));
+        }
+
 
         if(StringUtils.isNotBlank(this.SellingId)) {
             sbd.append(" AND selling.sellingId=?");
