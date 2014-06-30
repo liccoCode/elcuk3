ba8a6eb7493fc168fccf8f15d4bdb4468169fa9e
caryeasya <cary@easya.cc>
2014-06-30 12:09:46 +0800

    修改利润报表

--- core/app/models/view/post/ProfitPost.java
+++ core/app/models/view/post/ProfitPost.java
@@ -144,36 +144,35 @@ public class ProfitPost extends Post<Profit> {
 
                 if(skuprofit.sku.equals(p.sku)) {
                     skuprofit = addProfit(skuprofit, p);
-                    p.sku = p.sku + String.valueOf(p.totalfee) + " " + skuprofit.totalfee;
                 } else {
-                    skuprofit.sku = skuprofit.sku + "合计1";
+                    skuprofit.sku = skuprofit.sku + "合计";
                     newprofits.add(skuprofit);
                     skuprofit = initPorfit();
                     skuprofit.sku = p.sku;
-                    skuprofit.memo = skuprofit.sku + "合计1";
+                    skuprofit.memo = skuprofit.sku + "合计";
                     skuprofit = addProfit(skuprofit, p);
                 }
 
                 newprofits.add(p);
             }
-            skuprofit.sku = skuprofit.sku + "合计1";
+            skuprofit.sku = skuprofit.sku + "合计";
             newprofits.add(skuprofit);
         }
 
 
         M[] marray = getMarket();
         Profit totalp = initPorfit();
-        totalp.sku = "所有合计1";
-        totalp.memo = "所有合计1";
+        totalp.sku = "所有合计";
+        totalp.memo = "所有合计";
         totalp.market = null;
         for(Profit p : profits) {
             addProfit(totalp, p);
         }
         for(M m : marray) {
             Profit mp = initPorfit();
-            mp.sku = m.label() + "合计1";
+            mp.sku = m.label() + "合计";
             mp.market = m;
-            mp.memo = m.label() + "合计1";
+            mp.memo = m.label() + "合计";
             for(Profit p : profits) {
                 if(p.market == m) {
                     mp = addProfit(mp, p);
