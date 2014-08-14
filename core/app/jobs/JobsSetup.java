9614fbe255a8a99b9891bb854f328e1797dfb622
caryeasya <cary@easya.cc>
2014-08-14 15:18:22 +0800

    修改job

--- core/app/jobs/JobsSetup.java
+++ core/app/jobs/JobsSetup.java
@@ -53,7 +53,7 @@ public class JobsSetup {
 
             //every(AmazonOrderItemDiscover.class, "30s");
             //every(AmazonOrderUpdateJob.class, "1h");
-            every(AmazonOrderFetchJob.class, "20mn");
+            //every(AmazonOrderFetchJob.class, "20mn");
             every(OrderInfoFetchJob.class, "1mn");
 
             // Amazon Review Job 的处理
