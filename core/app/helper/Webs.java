9588f28579a1ad2829c843e671cd42cdec759789
caryeasya <cary@easya.cc>
2014-09-10 11:50:24 +0800

    修改webs

--- core/app/helper/Webs.java
+++ core/app/helper/Webs.java
@@ -168,8 +168,8 @@ public class Webs {
 
             email.setFrom("support@easyacceu.com", "EasyAcc");
             email.setSSL(true);
-            email.setSslSmtpPort("587");
-            email.setHostName("ec2-107-21-238-216.compute-1.amazonaws.com");
+            email.setSslSmtpPort("465");
+            email.setHostName("email-smtp.us-east-1.amazonaws.com");
 
             email.setAuthentication("AKIAJK2EC5XKMEZLW2SQ","ApTdBH8QIUJ5fLB/j62kwmEjcmXMzxdwCN5bdh79fJRf");
             email.setHtmlMsg(content);
