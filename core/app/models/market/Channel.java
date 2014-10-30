95886f22513f61a09b2ea232d7eaf2cd103c17cb
duan <duan@easya.cc>
2014-10-30 14:52:12 +0800

    * 新增 Channel 渠道 Model

---
+++ core/app/models/market/Channel.java
@@ -0,0 +1,16 @@
+package models.market;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+
+/**
+ * 渠道
+ * Created by IntelliJ IDEA.
+ * User: mac
+ * Date: 14-10-29
+ * Time: PM3:31
+ */
+@Entity
+public class Channel extends Model {
+}
