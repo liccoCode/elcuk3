22496f1c76eb320a09d0b1c4f05704cacd6f6f15
caryeasya <cary@easya.cc>
2014-12-05 09:35:29 +0800

    增加运输丢失量报表

--- core/app/controllers/Shipments.java
+++ core/app/controllers/Shipments.java
@@ -144,7 +144,6 @@ public class Shipments extends Controller {
         ship.endShipByComputer();
         List<Cooperator> cooperators = Cooperator.shippers();
         ship.arryParamSetUP(Shipment.FLAG.STR_TO_ARRAY);
-        System.out.println("xxxxxxxxxxxxxxxx::::::::"+ship.items.get(0).lossqty);
         render(ship, cooperators);
     }
 
