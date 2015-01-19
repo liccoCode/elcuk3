359d023d9bd14627d7715687030101ed39e0efaf
duan <duan@easya.cc>
2015-01-19 17:57:04 +0800

    * Category Review 星级与中差评趋势功能挪到 PmDashboard 首页中

--- core/app/controllers/MarketAnalysis.java
+++ core/app/controllers/MarketAnalysis.java
@@ -1,21 +1,8 @@
 package controllers;
 
 import controllers.api.SystemOperation;
-import helper.Dates;
-import helper.J;
-import helper.Webs;
-import models.market.AmazonListingReview;
-import models.product.Category;
-import models.view.Ret;
-import models.view.highchart.HighChart;
-import org.joda.time.DateTime;
-import play.modules.excel.RenderExcel;
 import play.mvc.Controller;
 import play.mvc.With;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 /**
  * 市场分析控制器
@@ -38,61 +25,5 @@ public class MarketAnalysis extends Controller {
      */
     public static void tags() {
         render();
-    }
-
-    /**
-     * Review 星级与中差评率趋势
-     */
-    public static void reviewRecords() {
-        List<String> categories = Category.categoryIds();
-        Date from = DateTime.now().minusMonths(6).toDate();
-        Date to = DateTime.now().toDate();
-        render(from, to, categories);
-    }
-
-    /**
-     * Review 星级与中差评率趋势导出
-     */
-    public static void exportReviewRecords(Date from, Date to, String category) {
-        HighChart reviewRatingLine = AmazonListingReview.reviewRatingLine(from, to, category);
-        HighChart poorRatingLine = AmazonListingReview.poorRatingLine(from, to, category);
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
-        SimpleDateFormat fileNameFormatter = new SimpleDateFormat("yyyyMMdd");
-        request.format = "xls";
-        renderArgs.put(RenderExcel.RA_FILENAME,
-                String.format("%s-%s品线Review星级与中差评.xls", fileNameFormatter.format(from), fileNameFormatter.format(to)));
-        renderArgs.put(RenderExcel.RA_ASYNC, false);
-        renderArgs.put("dateFormat", formatter);
-        renderArgs.put("reviewRatingLine", reviewRatingLine);
-        renderArgs.put("poorRatingLine", poorRatingLine);
-        renderArgs.put("from", from);
-        renderArgs.put("to", to);
-        renderArgs.put("category", category);
-        renderArgs.put("dates", Dates.getAllSunday(from, to));
-        render();
-    }
-
-    /**
-     * Review 星级趋势图
-     */
-    public static void reviewRatingLine(Date from, Date to, String category) {
-        try {
-            HighChart chart = AmazonListingReview.reviewRatingLine(from, to, category);
-            renderJSON(J.json(chart));
-        } catch(Exception e) {
-            renderJSON(new Ret(Webs.E(e)));
-        }
-    }
-
-    /**
-     * Review 中差评趋势图
-     */
-    public static void poorRatingLine(Date from, Date to, String category) {
-        try {
-            HighChart chart = AmazonListingReview.poorRatingLine(from, to, category);
-            renderJSON(J.json(chart));
-        } catch(Exception e) {
-            renderJSON(new Ret(Webs.E(e)));
-        }
     }
 }
