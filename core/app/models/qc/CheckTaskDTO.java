51b0ec6c383e91bd9f38a0f10939bf9c1075caf8
licco <licco@easya.cc>
2015-05-08 17:56:45 +0800

    add qctInfo function

---
+++ core/app/models/qc/CheckTaskDTO.java
@@ -0,0 +1,41 @@
+package models.qc;
+
+/**
+ * Created by mac on 15-5-8.
+ */
+public class CheckTaskDTO {
+
+    /**
+     * 箱数
+     */
+    public String boxNum;
+
+    /**
+     * 个数
+     */
+    public String num;
+
+    /**
+     * 单箱重量
+     */
+    public Double singleBoxWeight;
+
+    /**
+     * 长
+     */
+    public Double length;
+
+    /**
+     * 宽
+     */
+    public Double width;
+
+    /**
+     * 高
+     */
+    public Double height;
+
+    public CheckTaskDTO(){
+
+    }
+}
