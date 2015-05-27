ecc39368dd183ce2a1c0946e51de69229cbf927f
caryeasya <cary@easya.cc>
2015-05-27 17:29:15 +0800

    edit checktask

--- core/app/models/qc/CheckTask.java
+++ core/app/models/qc/CheckTask.java
@@ -12,13 +12,14 @@ import models.procure.ProcureUnit;
 import models.procure.ShipItem;
 import models.procure.Shipment;
 import models.product.Whouse;
-import models.view.dto.ProductDTO;
+import models.view.dto.CheckTaskAQLDTO;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.apache.commons.lang.StringUtils;
 import play.data.validation.Validation;
+import play.db.helper.SqlSelect;
 import play.db.jpa.Model;
 
 import javax.persistence.*;
@@ -376,6 +377,65 @@ public class CheckTask extends Model {
     @Transient
     public List<CheckTaskDTO> tailBoxQctInfos = new ArrayList<CheckTaskDTO>();
 
+    /**
+     * 不合格数量
+     */
+    public int unqualifiedQty;
+
+    @Expose
+    public String ac;
+
+    @Expose
+    public String re;
+
+    /**
+     * 抽检方式
+     */
+    @Lob
+    @Expose
+    public String samplingTypes = "";
+
+    @Transient
+    public List<String> samplingType = new ArrayList<String>();
+
+    /**
+     * 送检次数
+     */
+    @Lob
+    @Expose
+    public String inspectionTimes = "";
+
+    @Transient
+    public List<String> inspectionTime = new ArrayList<String>();
+
+    /**
+     * AQL 严重
+     */
+    @Expose
+    public String cr;
+
+    /**
+     * AQL 主要
+     */
+    @Expose
+    public String ma;
+
+    /**
+     * AQL 次要
+     */
+    @Expose
+    public String mi;
+
+    /**
+     * AQL 不良描述
+     * JSON 格式: [{"badDesc": "aaa", inspectionResult: [检验结果1, 检验结果2]}]
+     */
+    @Transient
+    public List<CheckTaskAQLDTO> aqlBadDesc = new ArrayList<CheckTaskAQLDTO>();
+
+    @Lob
+    public String aqlBadDescs = "[]";
+
     public enum FLAG {
         ARRAY_TO_STR,
         STR_TO_ARRAY
@@ -390,16 +450,52 @@ public class CheckTask extends Model {
         if(flag.equals(FLAG.ARRAY_TO_STR)) {
             this.qcRequires = StringUtils.join(this.qcRequire, Webs.SPLIT);
             this.qcWays = StringUtils.join(this.qcWay, Webs.SPLIT);
+
+            this.standBoxQctInfo = J.json(this.fixNullStr(this.standBoxQctInfos));
+            this.tailBoxQctInfo = J.json(this.fixNullStr(this.tailBoxQctInfos));
+
+            this.samplingTypes = StringUtils.join(this.samplingType, Webs.SPLIT);
+            this.inspectionTimes = StringUtils.join(this.inspectionTime, Webs.SPLIT);
+            this.aqlBadDescs = J.json(this.fixNullStr(this.aqlBadDesc));
         } else {
             this.qcRequire = new ArrayList<String>();
             this.qcWay = new ArrayList<String>();
+            this.samplingType = new ArrayList<String>();
+            this.inspectionTime = new ArrayList<String>();
 
             String temp[] = StringUtils.splitByWholeSeparator(this.qcRequires, Webs.SPLIT);
             if(temp != null) Collections.addAll(this.qcRequire, temp);
 
             temp = StringUtils.splitByWholeSeparator(this.qcWays, Webs.SPLIT);
             if(temp != null) Collections.addAll(this.qcWay, temp);
+
+            temp = StringUtils.splitByWholeSeparator(this.samplingTypes, Webs.SPLIT);
+            if(temp != null) Collections.addAll(this.samplingType, temp);
+
+            temp = StringUtils.splitByWholeSeparator(this.inspectionTimes, Webs.SPLIT);
+            if(temp != null) Collections.addAll(this.inspectionTime, temp);
+
+            if(StringUtils.isNotBlank(this.standBoxQctInfo)) this.standBoxQctInfos = JSON.parseArray(this
+                    .standBoxQctInfo, CheckTaskDTO.class);
+            if(StringUtils.isNotBlank(this.tailBoxQctInfo)) this.tailBoxQctInfos = JSON.parseArray(this
+                    .tailBoxQctInfo, CheckTaskDTO.class);
+            if(StringUtils.isNotBlank(this.aqlBadDescs))
+                this.aqlBadDesc = JSON.parseArray(this.aqlBadDescs, CheckTaskAQLDTO.class);
         }
+    }
+
+    /**
+     * 对空字符进行处理
+     *
+     * @return
+     */
+    private List fixNullStr(List target) {
+        Iterator<Object> iterator = target.iterator();
+        while(iterator.hasNext()) {
+            Object o = iterator.next();
+            if(o == null) iterator.remove();
+        }
+        return target;
     }
 
     public void fetchSkucheck() {
@@ -459,7 +555,7 @@ public class CheckTask extends Model {
                 newtask.units = punit;
                 newtask.confirmstat = ConfirmType.UNCONFIRM;
                 newtask.checkstat = StatType.UNCHECK;
-                if(punit.cooperator!=null && punit.cooperator.qcLevel == Cooperator.L.MICRO) {
+                if(punit.cooperator != null && punit.cooperator.qcLevel == Cooperator.L.MICRO) {
                     //当合作伙伴的质检级别为微检，则质检方式默认为工厂自检 其他情况需要质检员手动选择
                     newtask.qcType = T.SELF;
                 }
@@ -481,7 +577,7 @@ public class CheckTask extends Model {
 
         //因为运输方式经常变化，需要重新检查一次
         List<Map<String, Object>> unchecktasks = DBUtils.rows("select id from CheckTask where "
-                +"  units_id in (" +
+                + "  units_id in (" +
                 "  select unit_id from ShipItem where shipment_id in (" +
                 "  select id from Shipment where type='EXPRESS'" +
                 "  )" +
@@ -490,13 +586,13 @@ public class CheckTask extends Model {
             checkwarehouse(unchecktasks);
         }
         unchecktasks = DBUtils.rows("select id from CheckTask where "
-                        +" not exists (select 1 from ShipItem where ShipItem.unit_id=CheckTask.units_id)"
-                        +" and exists (select 1 from ProcureUnit where "
-                        +" ProcureUnit.id=CheckTask.units_id and shipType='EXPRESS')"
-                        +" and checkstat='UNCHECK'");
-                if(unchecktasks.size() > 0) {
-                    checkwarehouse(unchecktasks);
-                }
+                + " not exists (select 1 from ShipItem where ShipItem.unit_id=CheckTask.units_id)"
+                + " and exists (select 1 from ProcureUnit where "
+                + " ProcureUnit.id=CheckTask.units_id and shipType='EXPRESS')"
+                + " and checkstat='UNCHECK'");
+        if(unchecktasks.size() > 0) {
+            checkwarehouse(unchecktasks);
+        }
 
 
         List<Map<String, Object>> tasks = DBUtils.rows("select id from CheckTask where shipwhouse_id is null");
@@ -693,6 +789,17 @@ public class CheckTask extends Model {
         this.qty = newCt.qty;
         this.pickqty = newCt.pickqty;
         this.checkor = newCt.checkor;
+
+        this.unqualifiedQty = newCt.unqualifiedQty;
+        if(newCt.samplingTypes != null) this.samplingTypes = newCt.samplingTypes;
+        if(newCt.samplingTypes != null) this.inspectionTimes = newCt.inspectionTimes;
+        if(newCt.ac != null) this.ac = newCt.ac;
+        if(newCt.re != null) this.re = newCt.re;
+        if(newCt.cr != null) this.cr = newCt.cr;
+        if(newCt.ma != null) this.ma = newCt.ma;
+        if(newCt.mi != null) this.mi = newCt.mi;
+        if(newCt.aqlBadDescs != null) this.aqlBadDescs = newCt.aqlBadDescs;
+
         if(newCt.dealway != null) this.dealway = newCt.dealway;
         if(newCt.startTime != null) this.startTime = newCt.startTime;
         if(newCt.endTime != null) this.endTime = newCt.endTime;
@@ -946,22 +1053,6 @@ public class CheckTask extends Model {
     }
 
     /**
-     * 对空字符进行处理
-     *
-     * @return
-     */
-    private List<CheckTaskDTO> fixNullStr(List<CheckTaskDTO> target) {
-        Iterator<CheckTaskDTO> iterator = target.iterator();
-        while(iterator.hasNext()) {
-            CheckTaskDTO c = iterator.next();
-            if(null == c) {
-                iterator.remove();
-            }
-        }
-        return target;
-    }
-
-    /**
      * 计算总箱数
      *
      * @return
@@ -1011,5 +1102,6 @@ public class CheckTask extends Model {
         }
         return totalWeight;
     }
+
 
 }
