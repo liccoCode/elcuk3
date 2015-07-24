0d1723bc522c9cc5b9d5805e7f60b521ece163a6
caryeasya <cary@easya.cc>
2015-07-24 13:47:05 +0800

    edit ordercheck

--- core/app/controllers/api/OrderCheck.java
+++ core/app/controllers/api/OrderCheck.java
@@ -1,16 +1,15 @@
 package controllers.api;
 
-import jobs.PmDashboard.PmDashboardFetchJob;
-import jobs.driver.GJob;
+import models.market.Listing;
+import models.market.OrderItem;
 import models.market.Orderr;
+import models.market.Selling;
 import models.view.Ret;
-import org.apache.commons.lang.StringUtils;
 
 import play.mvc.Controller;
 import play.mvc.With;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 /**
  * 检查订单是否存在
@@ -32,5 +31,64 @@ public class OrderCheck extends Controller {
             renderJSON(new Ret(true, "存在订单!"));
         } else
             renderJSON(new Ret(false, "不存在订单!"));
+    }
+
+    /**
+     * 通过 asin 查品线
+     * 通过订单号查品线
+     * 通过 sku 查品线
+     * 通过邮箱查品线
+     */
+    public static void categorycheck() {
+        String checkinfo = request.params.get("checkinfo");
+        String checktype = request.params.get("checktype");
+        if(checktype.equals("asin")) {
+            Selling sell = Selling.find("asin=?", checkinfo).first();
+            if(sell != null) {
+                String category = sell.sellingId.substring(0, 2);
+                renderJSON(new Ret(true, category));
+            } else {
+                renderJSON(new Ret(false, "asin不存在!"));
+            }
+        } else if(checktype.equals("order")) {
+            OrderItem item = OrderItem.find("order.orderid=?", checkinfo).first();
+            if(item != null) {
+                String category = item.selling.sellingId.substring(0, 2);
+                renderJSON(new Ret(true, category));
+            } else {
+                renderJSON(new Ret(false, "order不存在!"));
+            }
+        } else if(checktype.equals("sku")) {
+            Listing listing = Listing.find("product_sku=?", checkinfo).first();
+            if(listing != null) {
+                String category = listing.sellings.get(0).sellingId.substring(0, 2);
+                renderJSON(new Ret(true, category));
+            } else {
+                renderJSON(new Ret(false, "sku不存在!"));
+            }
+        } else if(checktype.equals("email")) {
+            if(!(checkinfo.indexOf("marketplace.amazon.com") > 0
+                    || checkinfo.indexOf("marketplace.amazon.co.uk") > 0
+                    || checkinfo.indexOf("marketplace.amazon.de") > 0
+                    || checkinfo.indexOf("marketplace.amazon.ca") > 0
+                    || checkinfo.indexOf("marketplace.amazon.jp") > 0
+                    || checkinfo.indexOf("marketplace.amazon.it") > 0
+                    || checkinfo.indexOf("marketplace.amazon.es") > 0
+                    || checkinfo.indexOf("marketplace.amazon.fr") > 0)) {
+                renderJSON(new Ret(false, "email格式不存在!"));
+            }
+            List<Orderr> orderrs = Orderr.find("email=?", checkinfo).fetch();
+            if(orderrs.size() > 1) {
+                renderJSON(new Ret(false, "订单大于1个!"));
+            }
+            if(orderrs.size() > 0) {
+                String category = orderrs.get(0).items.get(0).selling.sellingId.substring(0, 2);
+                renderJSON(new Ret(true, category));
+            } else {
+                renderJSON(new Ret(false, "email不存在!"));
+            }
+        } else {
+            renderJSON(new Ret(false, "类型不存在!"));
+        }
     }
 }
