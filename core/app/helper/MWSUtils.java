dc8946b562c35aa7b1d79d55d61aa65b719a3f2e
licco <licco@liccodeMacBook-Pro.local>
2015-11-19 11:21:38 +0800

    1.5重构

--- core/app/helper/MWSUtils.java
+++ core/app/helper/MWSUtils.java
@@ -2,30 +2,44 @@ package helper;
 
 import com.amazonaws.mws.MarketplaceWebService;
 import com.amazonaws.mws.model.*;
+import com.elcuk.jaxb.*;
 import com.google.common.collect.Lists;
 import models.market.Account;
 import models.market.Feed;
 import models.market.M;
 import models.market.Selling;
 import models.view.post.SellingAmzPost;
-import mws.product.*;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang.StringUtils;
 import play.Logger;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.lang.Override;
 import java.math.BigDecimal;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.math.BigInteger;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLEncoder;
+import java.security.*;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 
 /**
  * Created by licco on 15/11/16.
  */
 public class MWSUtils {
+
+    private static final String CHARACTER_ENCODING = "UTF-8";
+    final static String ALGORITHM = "HmacSHA256";
+
 
     public enum T {
         UPLOAD_PRODUCT {
@@ -53,9 +67,9 @@ public class MWSUtils {
         InputStream content = new ByteArrayInputStream(feed.content.getBytes());
         req.setFeedContent(content);
         Logger.info("#####" + DigestUtils.md5Hex(feed.content));
-        req.setContentMD5(DigestUtils.md5Hex(feed.content));
-        req.setContentType(ContentType.XML);
         try {
+            req.setContentMD5(MWSUtils.computeContentMD5Header(content));
+            req.setContentType(ContentType.XML);
             SubmitFeedResponse resp = service.submitFeed(req);
             return resp.getSubmitFeedResult().getFeedSubmissionInfo().getFeedSubmissionId();
         } catch(Exception e) {
@@ -63,14 +77,82 @@ public class MWSUtils {
         }
     }
 
+    private static String calculateStringToSignV2(
+            Map<String, String> parameters, String serviceUrl) throws SignatureException, URISyntaxException {
+        // Sort the parameters alphabetically by storing
+        // in TreeMap structure
+        Map<String, String> sorted = new TreeMap<String, String>();
+        sorted.putAll(parameters);
+
+        // Set endpoint value
+        URI endpoint = new URI(serviceUrl.toLowerCase());
+
+        // Create flattened (String) representation
+        StringBuilder data = new StringBuilder();
+        data.append("POST\n");
+        data.append(endpoint.getHost());
+        data.append("\n/");
+        data.append("\n");
+
+        Iterator<Map.Entry<String, String>> pairs = sorted.entrySet().iterator();
+        while(pairs.hasNext()) {
+            Map.Entry<String, String> pair = pairs.next();
+            if(pair.getValue() != null) {
+                data.append(pair.getKey() + "=" + pair.getValue());
+            } else {
+                data.append(pair.getKey() + "=");
+            }
+            if(pairs.hasNext()) {
+                data.append("&");
+            }
+        }
+        return data.toString();
+    }
+
+    /*
+     * Sign the text with the given secret key and convert to base64
+     */
+    private static String sign(String data, String secretKey) throws NoSuchAlgorithmException, InvalidKeyException,
+            IllegalStateException, UnsupportedEncodingException {
+        Mac mac = Mac.getInstance(ALGORITHM);
+        mac.init(new SecretKeySpec(secretKey.getBytes(CHARACTER_ENCODING),
+                ALGORITHM));
+        byte[] signature = mac.doFinal(data.getBytes(CHARACTER_ENCODING));
+        String signatureBase64 = new String(Base64.encodeBase64(signature),
+                CHARACTER_ENCODING);
+        return new String(signatureBase64);
+    }
+
+    private static String urlEncode(String rawValue) {
+        String value = (rawValue == null) ? "" : rawValue;
+        String encoded = null;
+
+        try {
+            encoded = URLEncoder.encode(value, CHARACTER_ENCODING)
+                    .replace("+", "%20")
+                    .replace("*", "%2A")
+                    .replace("%7E", "~");
+        } catch(UnsupportedEncodingException e) {
+            System.err.println("Unknown encoding: " + CHARACTER_ENCODING);
+            e.printStackTrace();
+        }
+        return encoded;
+    }
+
     public static String computeContentMD5Header(InputStream inputStream) throws NoSuchAlgorithmException, IOException {
         DigestInputStream s;
         try {
-            s = new DigestInputStream(inputStream, MessageDigest.getInstance("MD5"));
+            DigestInputStream dis = new DigestInputStream(inputStream, MessageDigest.getInstance("MD5"));
             byte[] buffer = new byte[8192];
-            while(s.read(buffer) > 0) ;
-            s.getMessageDigest().update(buffer);
-            return new String(org.apache.commons.codec.binary.Base64.encodeBase64(s.getMessageDigest().digest()));
+            while(dis.read(buffer) > 0) ;
+
+            String md5Content = new String(org.apache.commons.codec.binary.Base64.encodeBase64(
+                    dis.getMessageDigest().digest()));
+
+            // Effectively resets the stream to be beginning of the file
+            // via a FileChannel.
+
+            return md5Content;
         } catch(NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         } catch(IOException e) {
@@ -111,6 +193,18 @@ public class MWSUtils {
     }
 
     public static String buildXMLBySelling(Selling selling, SellingAmzPost p) {
+        AmazonEnvelope envelope = new AmazonEnvelope();
+        Header header = new Header();
+        header.setDocumentVersion("1.01");
+        header.setMerchantIdentifier(selling.market.toMerchantIdentifier());
+        envelope.setHeader(header);
+        envelope.setMessageType("Product");
+        envelope.setPurgeAndReplace(false);
+
+        AmazonEnvelope.Message message = new AmazonEnvelope.Message();
+        message.setMessageID(BigInteger.valueOf(1));
+        message.setOperationType("PartialUpdate");
+
         Product product = new Product();
         product.setSKU(selling.merchantSKU);
         StandardProductID standardProductID = new StandardProductID();
@@ -171,7 +265,9 @@ public class MWSUtils {
             }
         }
         product.setDescriptionData(data);
-        String xml = JaxbUtil.convertToXml(product);
+        message.setProduct(product);
+        envelope.getMessage().add(message);
+        String xml = JaxbUtil.convertToXml(envelope);
         return xml;
     }
 
