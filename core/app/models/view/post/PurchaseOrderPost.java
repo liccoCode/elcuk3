c15a1482c8e7886edc565f96d69de833b4c6de39
licco <licco@liccodeMacBook-Pro.local>
2016-05-16 15:43:41 +0800

    采购明细报表开发

--- core/app/models/view/post/PurchaseOrderPost.java
+++ core/app/models/view/post/PurchaseOrderPost.java
@@ -1,12 +1,17 @@
 package models.view.post;
 
+import helper.DBUtils;
 import helper.Dates;
+import models.finance.Payment;
 import models.procure.ProcureUnit;
+import models.view.dto.PurchasePaymentDTO;
 import org.apache.commons.lang.StringUtils;
 import play.libs.F;
 
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by licco on 16/3/7.
@@ -51,5 +56,72 @@ public class PurchaseOrderPost extends Post<ProcureUnit> {
         return ProcureUnit.find(params._1, params._2.toArray()).fetch();
     }
 
+    public List<PurchasePaymentDTO> downloadReport() {
+        List<PurchasePaymentDTO> list = new ArrayList<>();
+        List<Object> params = new ArrayList<Object>();
+        StringBuilder sql = new StringBuilder();
+        sql.append(" SELECT c.name, u.deliveryment_id, u.id, u.sku, u.stage, u.price, u.currency, ");
+        sql.append(" IF(u.qty, u.qty,  u.planQty) AS qty, ");
+        sql.append(" round(sum(IF(u.qty, u.qty,  u.planQty) * u.price), 2) AS 'totalPurchases',");
+        sql.append(" IF(round(sum(p.amount + p.fixValue),2), round(sum(p.amount + p.fixValue)), 0) AS 'paymentAmount'");
+        sql.append(" FROM  ProcureUnit u ");
+        sql.append(" LEFT JOIN Deliveryment d ON d.id = u.deliveryment_id ");
+        sql.append(" LEFT JOIN Cooperator c ON c.id = u.cooperator_id ");
+        sql.append(" LEFT JOIN PaymentUnit p ON p.procureUnit_id = u.id ");
+        sql.append(" WHERE u.stage <> 'CLOSE'");
+        sql.append(" AND d.createDate >= ? ");
+        sql.append(" AND d.createDate <= ? ");
+        params.add(from);
+        params.add(to);
+        if(stage != null) {
+            sql.append(" AND u.stage = ? ");
+            params.add(stage);
+        }
+        if(cooperatorId != null) {
+            sql.append(" AND u.cooperator_id = ? ");
+            params.add(cooperatorId);
+        }
 
+        sql.append(" GROUP BY u.id, u.deliveryment_id ");
+        sql.append(" ORDER BY u.createDate DESC ");
+
+
+
+        List<Map<String, Object>> rows = DBUtils.rows(sql.toString(), params.toArray());
+        for(Map<String, Object> row : rows) {
+            PurchasePaymentDTO dto = new PurchasePaymentDTO();
+            dto.cooperator = row.get("name").toString();
+            dto.deliverymentId = row.get("deliveryment_id") == null ? "" : row.get("deliveryment_id").toString();
+            dto.unitId = row.get("id").toString();
+            dto.sku = row.get("sku").toString();
+            dto.stage = ProcureUnit.STAGE.valueOf(row.get("stage").toString());
+            dto.price = Float.valueOf(row.get("price").toString());
+            dto.currency = row.get("currency").toString();
+            dto.qty = Integer.valueOf(row.get("qty").toString());
+            dto.totalPurchases = Float.valueOf(row.get("totalPurchases").toString());
+            dto.paymentAmount = Float.valueOf(row.get("paymentAmount").toString());
+            dto.notPayAmount = dto.totalPurchases - dto.paymentAmount;
+            dto.payment = returnPayment(dto.deliverymentId);
+
+            list.add(dto);
+        }
+        return list;
+    }
+
+    public String returnPayment(String id) {
+
+        StringBuilder sql = new StringBuilder("SELECT p FROM Payment p ");
+        sql.append(" LEFT JOIN p.pApply a ");
+        sql.append(" LEFT JOIN a.deliveryments s ");
+        sql.append(" WHERE s.id = ? ");
+        List<Payment> list = Payment.find(sql.toString(), id).fetch();
+        if(list != null && list.size() > 0) {
+            String temp = "";
+            for(Payment p : list) {
+                temp += p.paymentNumber + " " + p.currency.symbol() + " " + p.actualPaid + ";  ";
+            }
+            return temp;
+        }
+        return "";
+    }
 }
