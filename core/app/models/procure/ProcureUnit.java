2b8e3198e626c16800d8bf7b5547ef7d4e9e18bc
duan <duan@easya.cc>
2016-06-13 14:29:12 +0800

    *  stages 删除 APPROVE

--- core/app/models/procure/ProcureUnit.java
+++ core/app/models/procure/ProcureUnit.java
@@ -25,6 +25,7 @@ import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import play.data.validation.Check;
 import play.data.validation.CheckWith;
 import play.data.validation.Required;
@@ -197,7 +198,7 @@ public class ProcureUnit extends Model implements ElcukRecord.Log {
 
 
     @OneToMany(mappedBy = "procureUnit", fetch = FetchType.LAZY)
-    public List<PaymentUnit> fees = new ArrayList<PaymentUnit>();
+    public List<PaymentUnit> fees = new ArrayList<>();
 
     /**
      * 此采购计划的供应商信息.
@@ -225,7 +226,7 @@ public class ProcureUnit extends Model implements ElcukRecord.Log {
      * 所关联的运输出去的 ShipItem.
      */
     @OneToMany(mappedBy = "unit")
-    public List<ShipItem> shipItems = new ArrayList<ShipItem>();
+    public List<ShipItem> shipItems = new ArrayList<>();
 
 
     @OneToOne(fetch = FetchType.LAZY)
@@ -1669,5 +1670,9 @@ public class ProcureUnit extends Model implements ElcukRecord.Log {
             }
         }
         return "暂无出库信息.";
+    }
+
+    public static STAGE[] stages() {
+        return ArrayUtils.removeElement(STAGE.values(), STAGE.APPROVE);
     }
 }
