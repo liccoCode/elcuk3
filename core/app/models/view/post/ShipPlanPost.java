fc26c627f084897c4c96c001fc0a89accebd6f75
duan <duan@easya.cc>
2016-07-20 17:58:05 +0800

    *  完成 ShipPlan 搜索

--- core/app/models/view/post/ShipPlanPost.java
+++ core/app/models/view/post/ShipPlanPost.java
@@ -1,9 +1,14 @@
 package models.view.post;
 
+import helper.Dates;
+import models.procure.Shipment;
 import models.whouse.ShipPlan;
+import org.apache.commons.lang.StringUtils;
+import org.joda.time.DateTime;
 import play.libs.F;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -18,17 +23,75 @@ public class ShipPlanPost extends Post<ShipPlan> {
     static {
         DATE_TYPES = new ArrayList<>();
         DATE_TYPES.add(new F.T2<>("createDate", "创建时间"));
-        DATE_TYPES.add(new F.T2<>("createDate", "预计运输时间"));
-        DATE_TYPES.add(new F.T2<>("completeDate", "预计到达时间"));
+        DATE_TYPES.add(new F.T2<>("planShipDate", "预计运输时间"));
+        DATE_TYPES.add(new F.T2<>("planArrivDate", "预计到达时间"));
+    }
+
+    public Date from;
+    public Date to;
+    public String dateType = "planShipDate";
+    public ShipPlan.S state;
+    public long whouseId;
+    public Shipment.T shipType;
+    public String search;
+
+    public ShipPlanPost() {
+        this.from = DateTime.now().minusDays(25).toDate();
+        this.to = new Date();
+        this.dateType = "planShipDate";
+        this.perSize = 50;
     }
 
     public List<ShipPlan> query() {
-        List<ShipPlan> list = new ArrayList<>();
-        return list;
+        F.T2<String, List<Object>> params = params();
+        this.count = this.count();
+        return ShipPlan.find(params._1, params._2.toArray()).fetch(this.page, this.perSize);
+    }
+
+    public Long getTotalCount() {
+        return this.count();
+    }
+
+    @Override
+    public Long count(F.T2<String, List<Object>> params) {
+        return ShipPlan.count(params._1, params._2.toArray());
     }
 
     @Override
     public F.T2<String, List<Object>> params() {
-        return null;
+        StringBuilder sbd = new StringBuilder("1=1");
+        List<Object> params = new ArrayList<>();
+
+        if(StringUtils.isNotBlank(this.search)) {
+            sbd.append(" AND (id LIKE ? OR selling.sellingId LIKE ? OR product.sku LIKE ? OR fba.shipmentId LIKE ? OR " +
+                    "product.abbreviation LIKE ?)");
+            String word = this.word();
+            for(int i = 0; i <= 4; i++) params.add(word);
+        }
+
+        if(StringUtils.isNotBlank(this.dateType)) {
+            sbd.append(" AND ").append(this.dateType).append(">=?").append(" AND ").append(this.dateType)
+                    .append("<=?");
+            params.add(Dates.morning(this.from));
+            params.add(Dates.night(this.to));
+        }
+
+        if(this.state != null) {
+            sbd.append(" AND state=?");
+            params.add(this.state.name());
+        }
+
+        if(this.whouseId > 0) {
+            sbd.append(" AND whouse.id=?");
+            params.add(this.whouseId);
+        }
+
+        if(this.shipType != null) {
+            sbd.append(" AND shipType=? ");
+            params.add(this.shipType.name());
+        }
+
+        sbd.append(" ORDER BY createDate DESC");
+        return new F.T2<>(sbd.toString(), params);
     }
 }
