cd4e1062a6346cd5bd3c32b1dd6c3e6a8f2fd4e1
duan <duan@easya.cc>
2016-08-23 12:38:29 +0800

    *  考虑到 ajaxUnit 使用 synchronized 来加锁进行计算, 所以还是加上 await *  取消 OrderItem#ajaxHighChartUnitOrder 使用多线程的方式来计算

--- core/app/helper/Promises.java
+++ core/app/helper/Promises.java
@@ -20,7 +20,7 @@ import java.util.concurrent.*;
 public class Promises {
     public static final M[] MARKETS = {M.AMAZON_DE, M.AMAZON_US, M.AMAZON_CA, M.AMAZON_UK, M.AMAZON_FR, M.AMAZON_ES,
             M.AMAZON_IT, M.AMAZON_JP};
-    private static final ExecutorService threadPool = Executors.newFixedThreadPool(MARKETS.length);
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(8);
 
     /**
      * Fork 多个 Job 根据 Callback.doJobWithResult(m) 去执行计算;
