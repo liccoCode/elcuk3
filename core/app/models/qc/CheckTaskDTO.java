c1247e0d4111e45f9d920ee925135474fd8cc89d
duan <duan@easya.cc>
2016-09-01 13:55:27 +0800

    *  CheckTaskDTO 增加可选的 lastCartonNum 属性

--- core/app/models/qc/CheckTaskDTO.java
+++ core/app/models/qc/CheckTaskDTO.java
@@ -23,6 +23,13 @@ public class CheckTaskDTO implements Serializable {
     public int num;
 
     /**
+     * 尾箱箱内的产品个数(可选)
+     * <p>
+     * PS: 如果尾箱的数据与主箱完全一致, 差别仅在于数量则可以使用该属性来传递数据
+     */
+    public Integer lastCartonNum;
+
+    /**
      * 单箱重量
      */
     public double singleBoxWeight;
@@ -42,7 +49,7 @@ public class CheckTaskDTO implements Serializable {
      */
     public double height;
 
-    public CheckTaskDTO(){
+    public CheckTaskDTO() {
 
     }
 }
