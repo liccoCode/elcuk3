be09a085d9c94215fc6207b15ede1ff212ab1256
licco <licco@liccodemacbook-pro.local>
2016-12-13 15:32:41 +0800

    退货单打印功能，以及功能优化

--- core/app/controllers/Refunds.java
+++ core/app/controllers/Refunds.java
@@ -3,14 +3,21 @@ package controllers;
 import controllers.api.SystemOperation;
 import models.procure.Cooperator;
 import models.view.post.RefundPost;
+import models.whouse.InboundUnit;
 import models.whouse.Refund;
+import models.whouse.RefundUnit;
 import models.whouse.Whouse;
+import org.allcolor.yahp.converter.IHtmlToPdfTransformer;
 import play.db.helper.SqlSelect;
+import play.modules.pdf.PDF;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
 
 import java.util.List;
+import java.util.Map;
+
+import static play.modules.pdf.PDF.renderPDF;
 
 /**
  * Created by licco on 2016/11/28.
@@ -47,6 +54,14 @@ public class Refunds extends Controller {
         Refund.confirmRefund(ids);
         flash.success(SqlSelect.inlineParam(ids) + "出库成功!");
         index(new RefundPost());
+    }
+
+    public static void printRefundForm(List<String> ids) {
+        final PDF.Options options = new PDF.Options();
+        options.pageSize = IHtmlToPdfTransformer.A4L;
+        List<Refund> refunds = Refund.find("id IN " + SqlSelect.inlineParam(ids)).fetch();
+        Map<Integer, List<RefundUnit>> ten = RefundUnit.pageNumForTen(refunds);
+        renderPDF(options, ten);
     }
 
 }
--- core/app/models/whouse/RefundUnit.java
+++ core/app/models/whouse/RefundUnit.java
@@ -1,13 +1,16 @@
 package models.whouse;
 
+import com.alibaba.fastjson.JSON;
+import helper.J;
 import models.procure.ProcureUnit;
+import models.qc.CheckTaskDTO;
 import org.hibernate.annotations.DynamicUpdate;
 import play.db.jpa.Model;
 
-import javax.persistence.Entity;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by licco on 2016/11/25.
@@ -32,7 +35,6 @@ public class RefundUnit extends Model {
      */
     public int qty;
 
-
     /**
      * 主箱信息
      */
@@ -45,7 +47,38 @@ public class RefundUnit extends Model {
     @Lob
     public String lastBoxInfo;
 
+    @Transient
+    public CheckTaskDTO mainBox = new CheckTaskDTO();
+
+    @Transient
+    public CheckTaskDTO lastBox = new CheckTaskDTO();
 
 
+    public static Map<Integer, List<RefundUnit>> pageNumForTen(List<Refund> list) {
+        Map<Integer, List<RefundUnit>> ten = new HashMap<>();
+        int k = 0;
+        for(Refund refund : list) {
+            List<RefundUnit> ru = refund.unitList;
+            int max = ru.size();
+            for(int i = 0; i < ru.size(); i += 10) {
+                int num = max - i;
+                ten.put(k, ru.subList(i, num > 10 ? i + 10 : i + num));
+                k++;
+            }
+        }
+        return ten;
+    }
+
+    @PostLoad
+    public void postPersist() {
+        this.mainBox = JSON.parseObject(this.mainBoxInfo, CheckTaskDTO.class);
+        this.lastBox = JSON.parseObject(this.lastBoxInfo, CheckTaskDTO.class);
+    }
+
+
+    public void marshalBoxs() {
+        this.mainBoxInfo = J.json(this.mainBox);
+        this.lastBoxInfo = J.json(this.lastBox);
+    }
 
 }
