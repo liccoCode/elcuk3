65ee3ff92edcf8af23459fa23b867f3e94e7152f
licco <licco@liccodemacbook-pro.local>
2016-12-28 16:49:36 +0800

    出库单页面查询功能修改

--- core/app/models/view/post/OutboundPost.java
+++ core/app/models/view/post/OutboundPost.java
@@ -2,17 +2,22 @@ package models.view.post;
 
 import helper.Dates;
 import models.whouse.Outbound;
+import org.apache.commons.lang.StringUtils;
 import org.joda.time.DateTime;
 import play.libs.F;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by licco on 2016/11/30.
  */
 public class OutboundPost extends Post<Outbound> {
 
+    private static final Pattern ID = Pattern.compile("^PTC(\\|\\d{6}\\|\\d+)$");
+    private static final Pattern NUM = Pattern.compile("^[0-9]*$");
     public Outbound.S status;
     public Outbound.T type;
 
@@ -29,17 +34,35 @@ public class OutboundPost extends Post<Outbound> {
 
     @Override
     public F.T2<String, List<Object>> params() {
-        StringBuilder sbd = new StringBuilder("1=1");
+        StringBuilder sbd = new StringBuilder("SELECT DISTINCT o FROM Outbound o LEFT JOIN o.units u WHERE 1=1");
         List<Object> params = new ArrayList<>();
+        sbd.append(" AND o.createDate >= ? AND o.createDate <= ? ");
+        params.add(Dates.morning(this.from));
+        params.add(Dates.night(this.to));
+
+        if(StringUtils.isNotEmpty(this.search)) {
+            if(StringUtils.isNotEmpty(isSearchForId())) {
+                sbd.append(" AND o.id = ? ");
+                params.add(isSearchForId());
+                return new F.T2<>(sbd.toString(), params);
+            }
+            if(isNumForSearch() != null) {
+                sbd.append(" AND o.unit.id = ? ");
+                params.add(isNumForSearch());
+                return new F.T2<>(sbd.toString(), params);
+            }
+            sbd.append(" AND o.unit.sku LIKE ? ");
+            params.add("%" + this.search + "%");
+        }
         if(status != null) {
-            sbd.append(" AND status = ? ");
+            sbd.append(" AND o.status = ? ");
             params.add(this.status);
         }
         if(type != null) {
-            sbd.append(" AND type = ? ");
+            sbd.append(" AND o.type = ? ");
             params.add(this.type);
         }
-        sbd.append(" ORDER BY createDate DESC");
+        sbd.append(" ORDER BY o.createDate DESC");
         return new F.T2<>(sbd.toString(), params);
     }
 
@@ -58,8 +81,23 @@ public class OutboundPost extends Post<Outbound> {
 
     @Override
     public Long count(F.T2<String, List<Object>> params) {
-        return Outbound.count(params._1, params._2.toArray());
+        return (long) Outbound.find(params._1, params._2.toArray()).fetch().size();
     }
 
+    private String isSearchForId() {
+        if(StringUtils.isNotBlank(this.search)) {
+            Matcher matcher = ID.matcher(this.search);
+            if(matcher.find()) return matcher.group(0);
+        }
+        return null;
+    }
+
+    private Long isNumForSearch() {
+        if(StringUtils.isNotBlank(this.search)) {
+            Matcher matcher = NUM.matcher(this.search);
+            if(matcher.find()) return Long.parseLong(matcher.group(0));
+        }
+        return null;
+    }
 
 }
