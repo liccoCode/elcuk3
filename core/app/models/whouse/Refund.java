4a2a679cd4d221173fcfc63dd058171d6cefff32
licco <licco@easya.cc>
2017-02-13 15:27:43 +0800

    不良品转入以及退货添加日志功能

--- core/app/models/whouse/Refund.java
+++ core/app/models/whouse/Refund.java
@@ -306,7 +306,7 @@ public class Refund extends GenericModel {
         u.qty = qty;
         u.save();
         createStockRecord(u, StockRecord.T.Unqualified_Refund, memo);
-        new ERecordBuilder("refund.confirm").msgArgs(u.qty).fid(unitId).save();
+        new ERecordBuilder("refund.confirm").msgArgs(u.qty, memo).fid(unitId).save();
     }
 
     /**
