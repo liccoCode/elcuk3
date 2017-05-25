726682bf3f7b9dd220fad271f2a4d4375e75832d
licco <licco@easya.cc>
2017-05-25 18:10:55 +0800

    包材BOM的开发

--- core/app/controllers/Materials.java
+++ core/app/controllers/Materials.java
@@ -1,16 +1,20 @@
 package controllers;
 
+import helper.J;
 import models.User;
 import models.material.Material;
 import models.material.MaterialBom;
 import models.procure.Cooperator;
+import models.view.Ret;
 import models.view.post.MaterialBomPost;
 import models.view.post.MaterialPost;
+import org.apache.commons.lang.StringUtils;
 import play.mvc.Controller;
 import play.mvc.With;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -49,6 +53,11 @@ public class Materials extends Controller {
         edit(m.id);
     }
 
+    public static void updateBom(MaterialBom bom) {
+        bom.save();
+        editBom(bom.id);
+    }
+
     public static void deleteMaterial(Long id) {
         Material material = Material.findById(id);
         material.isDel = true;
@@ -67,6 +76,12 @@ public class Materials extends Controller {
         indexBom(new MaterialBomPost());
     }
 
+    public static void editBom(Long id) {
+        MaterialBom bom = MaterialBom.findById(id);
+        List<User> users = User.find("closed=?", false).fetch();
+        render(bom, users);
+    }
+
     public static void indexBom(MaterialBomPost p) {
         if(p == null) p = new MaterialBomPost();
         List<MaterialBom> boms = p.query();
@@ -76,21 +91,40 @@ public class Materials extends Controller {
     }
 
     public static void createBom(MaterialBom b) {
-
         b.creator = Login.current();
         b.createDate = new Date();
         b.updateDate = new Date();
-
         b.save();
-
         indexBom(new MaterialBomPost());
     }
 
-    public static void showMaterials(String search) {
-
-        List<Material> materials = Material.find("").fetch();
-
+    public static void sameMaterial(String search) {
+        String word = String.format("%%%s%%", StringUtils.replace(search.trim(), "'", "''"));
+        List<Material> materials = Material.find("code like ? or name like ? ", word, word).fetch();
+        List<String> names = materials.stream().map(m -> m.name).collect(Collectors.toList());
+        renderJSON(J.json(names));
     }
 
+    public static void quickAddByMaterialName(String name, Long id) {
+        List<Material> materials = Material.find("name like ? ", name).fetch();
+        MaterialBom bom = MaterialBom.findById(id);
+        materials.forEach(m -> bom.materials.add(m));
+        bom.save();
+        renderJSON(new Ret(true));
+    }
+
+    /**
+     * 解除BOM和物料关系
+     *
+     * @param bomId
+     * @param id
+     */
+    public static void deleteRelationForBom(Long bomId, Long id) {
+        MaterialBom bom = MaterialBom.findById(bomId);
+        bom.materials.remove(Material.findById(id));
+        bom.save();
+        flash.success("解除关系成功！");
+        editBom(bomId);
+    }
 
 }
