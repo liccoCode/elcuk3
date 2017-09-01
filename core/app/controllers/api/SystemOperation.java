0ea6ec343440f77b2d214fdfd2942df6a8028ab7
licco <licco@easya.cc>
2017-09-01 11:55:57 +0800

    修改notify功能

--- core/app/controllers/api/SystemOperation.java
+++ core/app/controllers/api/SystemOperation.java
@@ -23,22 +23,7 @@ public class SystemOperation extends Controller {
     static void monitBefore() throws Throwable {
         //这里应该是用于记录整个应用所有 Controller 的数据统计信息.
         boolean isB2B = Objects.equals(OperatorConfig.getVal("brandname"), User.COR.MengTop.name());
-        Long id = Login.current().id;
-        List<Notification> personals = Notification.find("user.id=? AND state=?", id, Notification.S.UNCHECKED)
-                .fetch();
-        int num = personals.size();
-        List<Notification> systems = Notification.find("state=? AND type=?",
-                Notification.S.UNCHECKED, Notification.T.SYSTEM).fetch();
-        List<Notification> unReads = systems.stream().filter(system -> Notification.containUser(id, system.readUser))
-                .collect(Collectors.toList());
-        int totalUnReadNum = num + unReads.size();
-
-        renderArgs.put("personals", personals);
-        renderArgs.put("unReads", unReads);
-        renderArgs.put("totalUnReadNum", totalUnReadNum);
-
         renderArgs.put("isB2B", isB2B);
-
     }
 }
 
