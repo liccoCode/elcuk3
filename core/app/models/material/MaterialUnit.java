4c474767e851c0f13fc89aa3d402080826791c55
even <even@easya.cc>
2017-10-24 11:14:17 +0800

    物料首付款为0 由默认30%改为 需要设置供应商首付款

--- core/app/models/material/MaterialUnit.java
+++ core/app/models/material/MaterialUnit.java
@@ -409,14 +409,17 @@ public class MaterialUnit extends Model {
         if(this.hasTailPay()) {
             Validation.addError("", "已经申请了尾款, 不需要再申请预付款.");
         }
+        if(this.cooperator.first == 0) {
+            Validation.addError("", "当前预付款比例为空，请先设置供应商预付款比例！");
+        }
         if(Validation.hasErrors()) {
             return null;
         }
-
+        
         PaymentUnit fee = new PaymentUnit(this);
         // 预付款的逻辑在这里实现, 总额的 30% 为预付款
         fee.feeType = FeeType.cashpledge();
-        float pre = this.cooperator.first == 0 ? (float) 0.3 : (float) this.cooperator.first / 100;
+        float pre = (float) this.cooperator.first / 100;
         fee.amount = fee.amount * pre;
         fee.save();
         new ERecordBuilder("procureunit.prepay")
