66f44e11cc3dcd7e7008688f987e0913b76c1d35
licco <licco@easya.cc>
2017-11-22 16:29:18 +0800

    SQL语句修改

--- core/app/jobs/AmazonOrderFinanceFindJob.java
+++ core/app/jobs/AmazonOrderFinanceFindJob.java
@@ -17,14 +17,14 @@ import java.util.List;
 public class AmazonOrderFinanceFindJob extends BaseJob {
 
     public void doit() {
-            List<Orderr> orders = Orderr.find("AND o.synced=false AND feeflag=0 AND state IN ('SHIPPED','REFUNDED') "
-                    + "ORDER BY createDate DESC").fetch(20);
-            orders.forEach(order -> {
-                Logger.info("OrderId:" + order.orderId + " 开始执行 AmazonFinanceCheckJob 方法 ");
-                order.refreshFee();
-                order.synced = true;
-                order.feeflag = 2;
-                order.save();
-            });
-        }
+        List<Orderr> orders = Orderr.find("synced=false AND feeflag=0 AND state IN ('SHIPPED','REFUNDED') "
+                + "ORDER BY createDate DESC").fetch(20);
+        orders.forEach(order -> {
+            Logger.info("OrderId:" + order.orderId + " 开始执行 AmazonFinanceCheckJob 方法 ");
+            order.refreshFee();
+            order.synced = true;
+            order.feeflag = 2;
+            order.save();
+        });
+    }
 }
