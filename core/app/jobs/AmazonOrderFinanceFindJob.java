16b0b051d4a1ac191c4141f4a4f6ad47623f120b
licco <licco@easya.cc>
2017-11-22 16:23:56 +0800

    增加order费用job

---
+++ core/app/jobs/AmazonOrderFinanceFindJob.java
@@ -0,0 +1,30 @@
+package jobs;
+
+import jobs.driver.BaseJob;
+import models.market.Orderr;
+import play.Logger;
+import play.jobs.Every;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: licco
+ * Date: 2017/11/22
+ * Time: 下午4:22
+ */
+@Every("2mn")
+public class AmazonOrderFinanceFindJob extends BaseJob {
+
+    public void doit() {
+            List<Orderr> orders = Orderr.find("AND o.synced=false AND feeflag=0 AND state IN ('SHIPPED','REFUNDED') "
+                    + "ORDER BY createDate DESC").fetch(20);
+            orders.forEach(order -> {
+                Logger.info("OrderId:" + order.orderId + " 开始执行 AmazonFinanceCheckJob 方法 ");
+                order.refreshFee();
+                order.synced = true;
+                order.feeflag = 2;
+                order.save();
+            });
+        }
+}
