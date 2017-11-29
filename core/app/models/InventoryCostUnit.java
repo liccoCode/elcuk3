ddce1653e228a8f787d750616360d9b293188f3f
even <even@easya.cc>
2017-11-29 14:22:35 +0800

    * 库存资金报表  添加字段：在“入库中+在库成本”字段后添加“Reserved成本”字段内容 * 库存资金报表  “制作中、已交货成本”字段：计算方式变化：取制作中、已交货数量的相应采购计划ID上的数量*单价的总和 * 库存资金报表  “运输成本” 计算方式变化

--- core/app/models/InventoryCostUnit.java
+++ core/app/models/InventoryCostUnit.java
@@ -3,12 +3,10 @@ package models;
 import helper.DBUtils;
 import helper.Dates;
 import models.market.M;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import play.db.jpa.GenericModel;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.*;
 
@@ -81,6 +79,15 @@ public class InventoryCostUnit extends GenericModel {
      */
     public Date date;
 
+    /**
+     * 报表显示冗余字段,不做数据库映射
+     */
+    @Transient
+    public Float transportPriceTemp;
+    public Float taxPriceTemp;
+    
+
+
     public static List<Map<String, Object>> countByCategory(Date target) {
         StringBuilder sql = new StringBuilder(" select a.*,IFNULL(b.price,0) as productionCost from ( ");
         sql.append(" SELECT categoryId, `date`, ");
@@ -108,7 +115,7 @@ public class InventoryCostUnit extends GenericModel {
     }
 
 
-    public static List<Map<String, Object>> countPrice(
+    public static ImmutablePair<List<InventoryCostUnit>, List<Map<String, Object>>> countPrice(
             List<InventoryCostUnit> units, List<Map<String, Object>> summaries) {
         List<M> marketlist = Arrays.asList(M.AMAZON_DE, M.AMAZON_FR, M.AMAZON_UK, M.AMAZON_IT, M.AMAZON_ES);
 
@@ -118,32 +125,39 @@ public class InventoryCostUnit extends GenericModel {
 
         /** 1 循环明细集合,将 categoryId-sku-market : vat费用 存入map **/
         for(InventoryCostUnit unit : units) {
-            priceMap.put(String.format("%s-%s-%s", unit.categoryId, unit.sku, unit.market.name()), unit.taxPrice);
+            priceMap.put(String.format("taxPrice-%s-%s-%s", unit.categoryId, unit.sku, unit.market.name()),
+                    unit.taxPrice);
+            priceMap.put(String.format("transportPrice-%s-%s-%s", unit.categoryId, unit.sku, unit.market.name()),
+                    unit.transportPrice);
+            unit.transportPriceTemp = unit.transportPrice;
+            unit.taxPriceTemp = unit.taxPrice;
         }
 
         /** 2 循环明细集合,处理 欧洲五国vat费用为0的数据并按 品线汇总 存入map **/
         for(InventoryCostUnit unit : units) {
             if(unit.taxPrice == 0 && marketlist.contains(unit.market)) {
-                unit.taxPrice = getTaxPrice(priceMap, unit.categoryId, unit.sku, marketlist);
-                setTotalMap(unit, totalMap);
-            } else {
-                setTotalMap(unit, totalMap);
+                unit.taxPriceTemp = getTaxPrice(priceMap, unit.categoryId, unit.sku, marketlist);
             }
+            if(unit.transportPrice == 0 && marketlist.contains(unit.market)) {
+                unit.transportPriceTemp = getTransportPrice(priceMap, unit.categoryId, unit.sku, marketlist);
+            }
+            setTotalMap(unit, totalMap);
         }
 
         /** 3 汇总结果 匹配计算好的totalMap**/
         for(Map<String, Object> map : summaries) {
             map.put("stockCost", totalMap.get(String.format("%s-%s", map.get("categoryId"), "stockCost")));
             map.put("reservedCost", totalMap.get(String.format("%s-%s", map.get("categoryId"), "reservedCost")));
+            map.put("transitCost", totalMap.get(String.format("%s-%s", map.get("categoryId"), "transitCost")));
         }
-        return summaries;
+        return new ImmutablePair<>(units, summaries);
     }
 
 
     public static Float getTaxPrice(Map<String, Float> priceMap, String categoryId, String sku, List<M> marketlist) {
         Float taxPrice = 0f;
         for(M market : marketlist) {
-            taxPrice = priceMap.get(String.format("%s-%s-%s", categoryId, sku, market.name()));
+            taxPrice = priceMap.get(String.format("taxPrice-%s-%s-%s", categoryId, sku, market.name()));
             if(taxPrice != null && taxPrice > 0) {
                 break;
             }
@@ -152,22 +166,41 @@ public class InventoryCostUnit extends GenericModel {
         return taxPrice;
     }
 
+    public static Float getTransportPrice(Map<String, Float> priceMap, String categoryId, String sku,
+                                          List<M> marketlist) {
+        Float transportPrice = 0f;
+        for(M market : marketlist) {
+            transportPrice = priceMap.get(String.format("transportPrice-%s-%s-%s", categoryId, sku, market.name()));
+            if(transportPrice != null && transportPrice > 0) {
+                break;
+            }
+        }
+        if(transportPrice == null) return 0F;
+        return transportPrice;
+    }
 
     public static void setTotalMap(InventoryCostUnit unit, Map<String, BigDecimal> totalMap) {
-        BigDecimal price = new BigDecimal(unit.procurementPrice).add(new BigDecimal(unit.transportPrice)).add(new
-                BigDecimal(unit.taxPrice));
+        BigDecimal price = new BigDecimal(unit.procurementPrice).add(new BigDecimal(unit.transportPriceTemp)).add(new
+                BigDecimal(unit.taxPriceTemp));
         BigDecimal taxPrice = price.multiply(new BigDecimal(unit.stockQty));
         BigDecimal reservedPrice = price.multiply(new BigDecimal(unit.reservedQty));
 
+        BigDecimal transitCost = new BigDecimal(unit.procurementPrice).add(new BigDecimal(unit.transportPriceTemp))
+                .multiply(new BigDecimal(unit.transitQty));
+
+
         String taxPriceKey = String.format("%s-%s", unit.categoryId, "stockCost");
         String reservedPriceKey = String.format("%s-%s", unit.categoryId, "reservedCost");
+        String transitCostKey = String.format("%s-%s", unit.categoryId, "transitCost");
 
         if(totalMap.containsKey(taxPriceKey) && totalMap.containsKey(reservedPriceKey)) {
             totalMap.put(taxPriceKey, totalMap.get(taxPriceKey).add(taxPrice));
             totalMap.put(reservedPriceKey, totalMap.get(reservedPriceKey).add(reservedPrice));
+            totalMap.put(transitCostKey, totalMap.get(transitCostKey).add(transitCost));
         } else {
             totalMap.put(taxPriceKey, taxPrice);
             totalMap.put(reservedPriceKey, reservedPrice);
+            totalMap.put(transitCostKey, transitCost);
         }
     }
 }
