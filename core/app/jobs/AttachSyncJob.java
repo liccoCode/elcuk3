4c5d6aac3134244f383996de6d4e826121fe739c
even <even@easya.cc>
2018-01-04 18:09:13 +0800

    * 屏蔽迁移七牛云job

--- core/app/jobs/AttachSyncJob.java
+++ core/app/jobs/AttachSyncJob.java
@@ -17,7 +17,6 @@ import java.util.List;
  * Date: 2017/11/21
  * Time: 上午10:25
  */
-@Every("1mn")
 public class AttachSyncJob extends BaseJob {
 
     public void doit() {
@@ -26,10 +25,10 @@ public class AttachSyncJob extends BaseJob {
             Logger.info(String.format("Attach:[%s] 开始执行 七牛云迁移 方法", attach.location));
             try {
 
-                String bucket = String.format("%s-%s", models.OperatorConfig.getVal("brandname"), attach.p.name())
-                        .toLowerCase();
                 File tempFile = new File(attach.location);
-                String url = QiniuUtils.upload(tempFile.getName(), bucket, attach.getBytes());
+                String fileName = String.format("%s/%s", models.OperatorConfig.getVal("brandname"), tempFile.getName())
+                        .toLowerCase();
+                String url = QiniuUtils.upload(fileName, models.OperatorConfig.getVal("brandname"), attach.getBytes());
                 if(StringUtils.isNotBlank(url)) {
                     attach.qiniuLocation = url;
                     attach.sync = 1;
