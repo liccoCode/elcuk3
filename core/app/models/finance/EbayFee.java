eb89b29444ae50654e9c96855803fafdd4a0c5dd
licco <licco@easya.cc>
2018-02-27 15:58:44 +0800

    ebay订单重新抓取

--- core/app/models/finance/EbayFee.java
+++ core/app/models/finance/EbayFee.java
@@ -85,37 +85,47 @@ public class EbayFee extends Model {
         FinancialEvents financialEvents = result.getFinancialEvents();
         if(financialEvents.getShipmentEventList().size() > 0) {
             List<ShipmentEvent> events = financialEvents.getShipmentEventList();
-            events.forEach(event -> event.getShipmentItemList().forEach(item -> {
-                        item.getItemFeeList().forEach(
-                                component -> EbayFee.saveItemFeeForApi(component, event, item, "Order", order));
-                    }
-            ));
+            events.forEach(event -> {
+                event.getShipmentFeeList().forEach(fee -> EbayFee.saveShipmentFeeForApi(fee, event, order));
+                event.getShipmentItemList().forEach(item -> item.getItemFeeList().forEach(
+                        component -> EbayFee.saveItemFeeForApi(component, event, item, order)));
+            });
         }
         return true;
     }
 
 
-    private static void saveItemFeeForApi(FeeComponent component, ShipmentEvent event, ShipmentItem item, String type,
+    private static void saveItemFeeForApi(FeeComponent component, ShipmentEvent event, ShipmentItem item,
                                           EbayOrder order) {
         if(component.getFeeAmount().getCurrencyAmount().floatValue() != 0) {
             EbayFee fee = new EbayFee();
             fee.type = FeeType.findById(FeeType.mappingTypeName(component.getFeeType().toLowerCase()));
             fee.currency = Currency.valueOf(component.getFeeAmount().getCurrencyCode());
             fee.cost = component.getFeeAmount().getCurrencyAmount().floatValue();
-            EbayFee.commonField(fee, event, item, type, order);
+            EbayFee.commonField(fee, event, item, order);
         }
     }
 
-    private static void commonField(EbayFee fee, ShipmentEvent event, ShipmentItem item, String type, EbayOrder order) {
+    private static void saveShipmentFeeForApi(FeeComponent component, ShipmentEvent event, EbayOrder order) {
+        if(component.getFeeAmount().getCurrencyAmount().floatValue() != 0) {
+            EbayFee fee = new EbayFee();
+            fee.type = FeeType.findById(FeeType.mappingTypeName(component.getFeeType().toLowerCase()));
+            fee.currency = Currency.valueOf(component.getFeeAmount().getCurrencyCode());
+            fee.cost = component.getFeeAmount().getCurrencyAmount().floatValue();
+            EbayFee.commonField(fee, event, null, order);
+        }
+    }
+
+    private static void commonField(EbayFee fee, ShipmentEvent event, ShipmentItem item, EbayOrder order) {
         fee.usdCost = fee.currency.toUSD(fee.cost);
         fee.market = order.market;
         fee.memo = "ERP重新抓取Ebay费用 API";
         fee.orderId = event.getAmazonOrderId();
         fee.order = EbayOrder.findById(fee.orderId);
-        fee.qty = item.getQuantityShipped();
+        fee.qty = item != null ? item.getQuantityShipped() : 1;
         fee.date = event.getPostedDate().toGregorianCalendar().getTime();
-        fee.transaction_type = type;
-        fee.product_sku = item.getSellerSKU().split(",")[0];
+        fee.transaction_type = "Order";
+        fee.product_sku = item != null ? item.getSellerSKU().split(",")[0] : null;
         fee.account = order.account;
         Map<String, Object> map = new HashMap<>();
         map.put("order_orderId", fee.orderId);
@@ -126,7 +136,9 @@ public class EbayFee extends Model {
         map.put("cost", fee.cost.toString());
         map.put("currency", fee.currency.name());
         map.put("type_name", fee.type.name.toLowerCase());
-        map.put("product_sku", fee.product_sku);
+        if(fee.product_sku != null) {
+            map.put("product_sku", fee.product_sku);
+        }
         map.put("transaction_type", fee.transaction_type);
         fee.md5_id = Webs.md5ForSaleFee(map);
         fee.save();
