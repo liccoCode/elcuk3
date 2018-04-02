8c9a84113c3e8fff847c49ed06c030e500e1e67b
licco <licco@easya.cc>
2018-04-02 17:07:49 +0800

    添加每日库存保存功能

--- core/app/jobs/DailyStockJob.java
+++ core/app/jobs/DailyStockJob.java
@@ -1,11 +1,15 @@
 package jobs;
 
+import helper.Dates;
 import jobs.driver.BaseJob;
 import models.view.post.StockPost;
 import models.whouse.DailyStock;
+import models.whouse.StockRecord;
 import play.jobs.On;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,13 +23,23 @@ public class DailyStockJob extends BaseJob {
 
     @Override
     public void doit() {
+        Date now = new Date();
         StockPost p = new StockPost();
         Map<String, String> total = p.total();
         DailyStock stock = new DailyStock();
-        stock.date = new Date();
+        stock.date = now;
         stock.qty = Integer.parseInt(total.get("totalQty"));
         stock.totalCNY = Double.parseDouble(total.get("totalCNY"));
         stock.totalUSD = Double.parseDouble(total.get("totalUSD"));
+        List<StockRecord> recordList = StockRecord.find("createDate>=? AND createDate<=? AND type IN (?,?,?,?,?)",
+                Dates.morning(now), Dates.night(now), StockRecord.T.Inbound, StockRecord.T.Unqualified_Transfer,
+                StockRecord.T.Outbound, StockRecord.T.OtherOutbound, StockRecord.T.Refund).fetch();
+        stock.inboundQty = recordList.stream()
+                .filter(record -> Arrays.asList(StockRecord.T.Inbound, StockRecord.T.Unqualified_Transfer)
+                        .contains(record.type)).mapToInt(record -> record.qty).sum();
+        stock.outboundQty = recordList.stream().filter(record -> Arrays
+                .asList(StockRecord.T.Outbound, StockRecord.T.OtherOutbound, StockRecord.T.Refund).contains(record.type))
+                .mapToInt(record -> record.qty).sum();
         stock.save();
     }
 }
